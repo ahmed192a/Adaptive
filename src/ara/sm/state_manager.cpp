a3ce470cfac8e9e40aeabd15ef74b0afd00106ca
KhaledNagah <54414359+KhaledNagah@users.noreply.github.com>
2022-02-11 14:15:21 +0200

    Add files via upload

---
+++ src/ara/sm/state_manager.cpp
@@ -0,0 +1,16 @@
+#include "state_manager.h"
+
+using namespace ara::sm;
+
+void StateManager::init(void) {
+    // // ********* under discussion ******** ==> get our process id  ==> getPid();
+
+    // //  using the constructor and the preconstructor to append to the array
+    // //  check the implementation critiria of the function_group_state (if we should extract it manually in the SM)
+
+    // /** initial communication with EM **/
+    EM.GetInitialMachineStateTransitionResult();
+
+    
+
+}
---
+++ src/ara/sm/state_manager.h
@@ -0,0 +1,42 @@
+#ifndef ARA_SM_STATE_MANAGER_H
+#define ARA_SM_STATE_MANAGER_H
+
+#include <vector>
+#include <map>
+
+#include "../exec/function_group.h"
+#include "../exec/function_group_state.h"
+#include "../exec/state_client.h"
+
+namespace ara {
+    namespace sm {
+        
+        using FunctionGroup = ara::exec::FunctionGroup;
+        using FunctionGroupState = ara::exec::FunctionGroupState;
+
+
+        class StateManager {
+
+            public:
+
+            /** Defining the needed funcion groups **/
+            static FunctionGroup MachineFG; // The machine Function Group
+            static std::vector<FunctionGroupState> MachineStates; // The machine States
+
+            static std::vector<FunctionGroup,  std::vector<FunctionGroupState>> FunctionGroups; // The Other function groups
+
+       
+            
+            /** Execution Manager  connectivity API instance **/
+            static ara::exec::StateClient EM;
+
+
+            static void init(void);
+
+        }; // class StateManager
+        
+
+    } // namespace sm
+} // namespace ara
+
+#endif // ARA_SM_STATE_MANAGER_H
