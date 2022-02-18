26e9cba33c5f808db6a1508914ffe53cc4625a04
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-02-19 00:29:59 +0200

    Add files via upload

---
+++ src/ara/sm/triggerin_skeleton.h
@@ -0,0 +1,43 @@
+namespace ara 
+{
+namespace sm 
+{
+namespace triggerin
+{
+namespace skeleton
+{
+namespace events
+{
+
+}
+namespace fields
+{
+    using Trigger=int;
+    
+}
+class Trigger_In_Skeleton
+{
+    public:
+    Trigger_In_Skeleton(
+        ara::com::InstanceIdentifier instance,
+        ara::com::MethodCallProcessingMode mode = ara::com::MethodCallProcessingMode::kEvent
+    )
+    {
+      
+        //Events
+	
+        //Fields
+        //Field("Trigger",false,false,true);
+	
+        //Methods
+       
+
+        // init
+       // init();
+    }
+    
+};
+}
+}
+}
+}
