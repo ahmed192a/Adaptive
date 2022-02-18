21a3cb13745db9d70268eb54a79fd8c60a43019a
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-02-19 00:49:51 +0200

    Add files via upload

---
+++ src/ara/sm/triggerin_proxy.h
@@ -0,0 +1,47 @@
+namespace ara 
+{
+namespace sm
+{
+namespace trigger_in
+{
+namespace proxy
+{
+namespace events{}
+namespace fields
+{
+    using Trigger=int;
+    
+}
+namespace methods{}
+class Trigger_In_Skeleton
+{
+    public:
+   /* explicit TriggerInProxy()
+    	 Trigger_In_Skeleton(
+        ara::com::InstanceIdentifier instance,
+        ara::com::MethodCallProcessingMode mode = ara::com::MethodCallProcessingMode::kEvent
+        // , Trigger(this, "Trigger")
+    	
+    )
+    */
+    {
+        //Events
+	
+        //Fields
+        //Field("Trigger",false,false,true);
+	
+        //Methods
+       
+        
+		
+        // init
+       //init();
+    }
+      
+      
+    
+};
+}
+}
+}
+}
