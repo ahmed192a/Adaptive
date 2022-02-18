603b11da532a1ea51c9d022ced7d9d97ee68f9dc
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-02-19 01:10:45 +0200

    Add files via upload

---
+++ src/ara/sm/triggerout_skeleton.h
@@ -0,0 +1,43 @@
+namespace ara 
+{
+namespace sm 
+{
+namespace triggerout
+{
+namespace skeleton
+{
+namespace events
+{
+
+}
+namespace notifier
+{
+    using notifier=int;
+    
+}
+class Trigger_Out_Skeleton
+{
+    public:
+    Trigger_Out_Skeleton(
+       /* ara::com::InstanceIdentifier instance,
+        ara::com::MethodCallProcessingMode mode = ara::com::MethodCallProcessingMode::kEvent*/
+    )
+    {
+      
+        //Events
+	
+        //Fields
+        //Field("notifier",true,true,false);
+	
+        //Methods
+       
+
+        // init
+       // init();
+    }
+    
+};
+}
+}
+}
+}
