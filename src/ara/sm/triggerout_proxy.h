196446ac7c1bc0629a57b46eea4e376abb618bc8
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-02-19 01:18:41 +0200

    Add files via upload

---
+++ src/ara/sm/triggerout_proxy.h
@@ -0,0 +1,46 @@
+namespace ara 
+{
+namespace sm
+{
+namespace trigger_in
+{
+namespace proxy
+{
+namespace events{}
+namespace fields
+{
+    using notifier=int;
+    
+}
+namespace methods{}
+class Trigger_Out_Skeleton
+{
+    public:
+   /* explicit TriggerOutProxy()
+    	 Trigger_Out_Skeleton(
+        ara::com::InstanceIdentifier instance,
+        ara::com::MethodCallProcessingMode mode = ara::com::MethodCallProcessingMode::kEvent
+        // , notifier(this, "notifier")
+    	
+    )
+    */
+    {
+        //Events
+	
+        //Fields
+        //Field("notifier",true,true,false);
+        //Methods
+       
+        
+		
+        // init
+       //init();
+    }
+      
+      
+    
+};
+}
+}
+}
+}
