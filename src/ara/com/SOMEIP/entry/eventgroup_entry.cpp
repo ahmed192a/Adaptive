a2a74cc11e75e932e33b632f96c6c262e1dee79c
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-02-23 23:03:14 +0200

    Add files via upload

---
+++ src/ara/com/SOMEIP/entry/eventgroup_entry.cpp
@@ -0,0 +1,159 @@
+#include "eventgroup_entry.h"
+
+namespace ara
+{
+    namespace com
+    {
+        namespace entry
+        {
+            EventgroupEntry::EventgroupEntry(EntryType type,
+                                             uint16_t serviceId,
+                                             uint16_t instanceId,
+                                             uint32_t ttl,
+                                             uint8_t majorVersion,
+                                             uint16_t eventgroupId) noexcept : Entry(type, serviceId, instanceId, ttl, majorVersion),
+                                                                               GBEventgroupId{eventgroupId}
+            {
+            }
+
+            bool EventgroupEntry::isAcknowledge() const noexcept
+            {
+                bool _result =
+                    (Type() == EntryType::Acknowledging) && (TTL() > NackTTL);
+
+                return _result;
+            }
+
+            bool EventgroupEntry::ValidateOption(
+                const option::Option *option) const noexcept
+            {
+                bool _result = Entry::ValidateOption(option);
+
+                if (_result)
+                {
+                    switch (option->Type())
+                    {
+                    case option::OptionType::IPv4Endpoint:
+                    case option::OptionType::IPv6Endpoint:
+                    {
+                        // Endpoint option is allowed only eventgroup subscription entries.
+                        _result = (Type() == EntryType::Subscribing);
+                        break;
+                    }
+                    case option::OptionType::IPv4Multicast:
+                    case option::OptionType::IPv6Multicast:
+                    {
+                        // Multicast option is not allowed in eventgroup entries expect acknowledgement.
+                        if (isAcknowledge())
+                        {
+                            _result = !ContainsOption(option->Type());
+                        }
+                        else
+                        {
+                            _result = false;
+                        }
+
+                        break;
+                    }
+                    default:
+                    {
+                        _result = false;
+                        break;
+                    }
+                    }
+                }
+
+                return _result;
+            }
+
+            uint16_t EventgroupEntry::EventgroupId() const noexcept
+            {
+                return GBEventgroupId;
+            }
+
+            std::vector<uint8_t> EventgroupEntry::Payload(uint8_t &optionIndex) const
+            {
+                std::vector<uint8_t> _result = Entry::BasePayload(optionIndex);
+
+                // Enabled Inistal Data Request Flag without any counter
+                const uint16_t EventgroupFlag = 0x0080;
+                helper::Inject(_result, EventgroupFlag);
+                helper::Inject(_result, GBEventgroupId);
+
+                return _result;
+            }
+
+            EventgroupEntry EventgroupEntry::CreateSubscribeEventEntry(
+                uint16_t serviceId,
+                uint16_t instanceId,
+                uint8_t majorVersion,
+                uint16_t eventgroupId) noexcept
+            {
+                const EntryType SubscribeEventEntry = EntryType::Subscribing;
+                const uint32_t SubscribeEventTTL = 0xffffff;
+
+                EventgroupEntry _result(
+                    cSubscribeEventEntry,
+                    serviceId,
+                    instanceId,
+                    cSubscribeEventTTL,
+                    majorVersion,
+                    eventgroupId);
+
+                return _result;
+            }
+
+            EventgroupEntry EventgroupEntry::CreateUnsubscribeEventEntry(
+                uint16_t serviceId,
+                uint16_t instanceId,
+                uint8_t majorVersion,
+                uint16_t eventgroupId) noexcept
+            {
+                const EntryType cSubscribeEventEntry = EntryType::Subscribing;
+                const uint32_t cUnsubscribeEventTTL = 0x000000;
+
+                EventgroupEntry _result(
+                    cSubscribeEventEntry,
+                    serviceId,
+                    instanceId,
+                    cUnsubscribeEventTTL,
+                    majorVersion,
+                    eventgroupId);
+
+                return _result;
+            }
+
+            EventgroupEntry EventgroupEntry::CreateAcknowledgeEntry(
+                const EventgroupEntry &eventgroupEntry) noexcept
+            {
+                const EntryType AcknowledgetEntry = EntryType::Acknowledging;
+
+                EventgroupEntry _result(
+                    AcknowledgetEntry,
+                    eventgroupEntry.ServiceId(),
+                    eventgroupEntry.InstanceId(),
+                    eventgroupEntry.TTL(),
+                    eventgroupEntry.MajorVersion(),
+                    eventgroupEntry.EventgroupId());
+
+                return _result;
+            }
+
+            EventgroupEntry EventgroupEntry::CreateNegativeAcknowledgeEntry(
+                const EventgroupEntry &eventgroupEntry) noexcept
+            {
+                const EntryType AcknowledgetEntry = EntryType::Acknowledging;
+
+                EventgroupEntry _result(
+                    AcknowledgetEntry,
+                    eventgroupEntry.ServiceId(),
+                    eventgroupEntry.InstanceId(),
+                    NackTTL,
+                    eventgroupEntry.MajorVersion(),
+                    eventgroupEntry.EventgroupId());
+
+                return _result;
+            }
+        }
+    }
+}
---
+++ src/ara/com/SOMEIP/entry/service_entry.cpp
@@ -0,0 +1,116 @@
+#include "./service_entry.h"
+
+namespace ara
+{
+    namespace com
+    {
+        namespace entry
+        {
+            ServiceEntry::ServiceEntry(EntryType type,
+                                       uint16_t serviceId,
+                                       uint16_t instanceId,
+                                       uint32_t ttl,
+                                       uint8_t majorVersion,
+                                       uint32_t minorVersion) noexcept : Entry(type, serviceId, instanceId, ttl, majorVersion),
+                                                                         GBMinorVersion{minorVersion}
+            {
+            }
+
+            bool ServiceEntry::ValidateOption(
+                const option::Option *option) const noexcept
+            {
+                bool _result = Entry::ValidateOption(option);
+
+                // Multicast option is not allowed in service entries.
+                _result &=
+                    (option->Type() != option::OptionType::IPv4Multicast) &&
+                    (option->Type() != option::OptionType::IPv6Multicast);
+
+                return _result;
+            }
+
+            uint32_t ServiceEntry::MinorVersion() const noexcept
+            {
+                return GBMinorVersion;
+            }
+
+            std::vector<uint8_t> ServiceEntry::Payload(uint8_t &optionIndex) const
+            {
+                std::vector<uint8_t> _result = Entry::BasePayload(optionIndex);
+                helper::Inject(_result, GBMinorVersion);
+
+                return _result;
+            }
+
+            ServiceEntry ServiceEntry::CreateFindServiceEntry(
+                uint16_t serviceId,
+                uint32_t ttl,
+                uint16_t instanceId,
+                uint8_t majorVersion,
+                uint32_t minorVersion)
+            {
+                const EntryType cFindServiceEntry = EntryType::Finding;
+
+                if (ttl == 0)
+                {
+                    throw std::invalid_argument("TTL cannot be zero.");
+                }
+
+                ServiceEntry _result(
+                    cFindServiceEntry,
+                    serviceId,
+                    instanceId,
+                    ttl,
+                    majorVersion,
+                    minorVersion);
+
+                return _result;
+            }
+
+            ServiceEntry ServiceEntry::CreateOfferServiceEntry(
+                uint16_t serviceId,
+                uint16_t instanceId,
+                uint8_t majorVersion,
+                uint32_t minorVersion,
+                uint32_t ttl)
+            {
+                const EntryType cOfferServiceEntry = EntryType::Offering;
+
+                if (ttl == 0)
+                {
+                    throw std::invalid_argument("TTL cannot be zero.");
+                }
+
+                ServiceEntry _result(
+                    cOfferServiceEntry,
+                    serviceId,
+                    instanceId,
+                    ttl,
+                    majorVersion,
+                    minorVersion);
+
+                return _result;
+            }
+
+            ServiceEntry ServiceEntry::CreateStopOfferEntry(
+                uint16_t serviceId,
+                uint16_t instanceId,
+                uint8_t majorVersion,
+                uint32_t minorVersion) noexcept
+            {
+                const EntryType OfferServiceEntry = EntryType::Offering;
+                const uint32_t StopOfferTTL = 0x000000;
+
+                ServiceEntry _result(
+                    cOfferServiceEntry,
+                    serviceId,
+                    instanceId,
+                    cStopOfferTTL,
+                    majorVersion,
+                    minorVersion);
+
+                return _result;
+            }
+        }
+    }
+}
