39325cddd67f93498f801aaf0419a42decdbbe1c
Nourhan Ashraf <74120502+NourhanAshraf1999@users.noreply.github.com>
2022-03-01 21:29:45 +0200

    Update server_someip.cpp

--- test_partial_processes/server_someip.cpp
+++ test_partial_processes/server_someip.cpp
@@ -2,7 +2,20 @@
 
 
 #include <iostream>
+#include "ara/com/SOMEIP_MESSAGE/sd/SomeipSDMessage.h"
+#include<vector>
+#include"ara/com/option/ipv4_endpoint_option.h"
+#include"ara/com/helper/service_entry.h"
 using namespace std;
+using namespace ara::com::entry;
+using namespace ara::com::SOMEIP_MESSAGE::sd;
+using namespace ara::com::option;
+using namespace ara::com::helper;
+SomeIpSDMessage GBSD;
+ServiceEntry GBSE=ServiceEntry::CreateFindServiceEntry (1,2,3,10,11);
+Ipv4EndpointOption GBIP= Ipv4EndpointOption::CreateSdEndpoint(false,Ipv4Address(1,2,3,4),Layer4ProtocolType::Udp,2154);
+
+
 int main()
 {
     
@@ -15,9 +28,10 @@ int main()
     sd.sin_addr.s_addr = INADDR_ANY;
     sd.sin_port = htons(2033);
     char buffer [] = "some ip message";
-    //sd.sa_data;
-
-    servertest.UDPSendTo(&buffer, sizeof(buffer), (const sockaddr *)&sd);
+    GBSE.AddFirstOption(&GBIP);
+    GBSD.AddEntry(&GBSE);
+    vector<uint8_t>payload=GBSE.Payload();
+    servertest.UDPSendTo(&payload, sizeof(payload), (const sockaddr *)&sd);
     int x;
     cin>>x;
     return 0;
