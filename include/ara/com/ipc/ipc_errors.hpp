c882e565351c59ec047d1777e834538d82e509a6
ahmed mohamed <42156372+ahmed192a@users.noreply.github.com>
2022-03-15 00:32:31 +0200

    fix errors

--- include/ara/com/ipc/ipc_errors.hpp
+++ include/ara/com/ipc/ipc_errors.hpp
@@ -11,20 +11,32 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
-
+/// @brief sockets error code 
 enum error_kind
 {
+	/// (value 0x00 or 0) not set => Means no error 
 	SUCCEEDED,
+	/// (value 0x01 or 1) means that an error happened while openning socket
 	FAILEDSOCETOPEN,
+	/// (value 0x02 or 2) means that an error happened while binding to bort
 	BIND_FAILED,
+	/// (value 0x03 or 3) means that an error happened while listenning
 	LISTEN_FAILED,
+	/// (value 0x04 or 4) means that an error happened while accepting
 	ACCEPT_FAILED,
+	/// (value 0x05 or 5) means that an error happened while sending
 	SEND_FAILED,
+	/// (value 0x06 or 6) means that an error happened while receiving
 	RECEIVE_FAILED,
+	/// (value 0x07 or 7) means that an error happened while getting host
 	GETTINGHOST_FAILED,
+	/// (value 0x08 or 8) means that an error happened while trying to connect
 	CONNECTION_FAILED,
+	/// (value 0x09 or 9) means that an error happened while writing
 	WRITING_FAILED,
+	/// (value 0x0A or 10) means that an error happened while reading
 	READING_FAILED,
+	/// (value 0x0B or 11) means that general failure
 	FAILED
 };
 
