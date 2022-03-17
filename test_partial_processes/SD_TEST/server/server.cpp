f47091ab2b119b8fbb648af7a6765d1fa7ddfb2e
Basmala <59097918+BasmalaMagdy@users.noreply.github.com>
2022-03-17 14:17:42 +0200

    add thread

--- test_partial_processes/SD_TEST/server/server.cpp
+++ test_partial_processes/SD_TEST/server/server.cpp
@@ -15,11 +15,15 @@
 
 #include <sys/mman.h>
 #include <sys/ipc.h>
+#include <pthread.h>
+#include <cstdlib>
+#include <iostream>
 
 #define SERVER_PORT 5365
 #define SD_PORT 1690
 #define MAX_QUEUE_CLIENTS 5
 #define SERVICE_ID 32
+#define NUM_THREADS 3
 
 using namespace std;
 void Handle_IO();
@@ -34,104 +38,127 @@ skeleton server_skeleton_obj(instance, skeleton_handle);
 
 static skeleton *server_skeleton_ptr;
 
+void *pthread0(void *v_var)
+{
+    CServer server_main_socket(SOCK_STREAM);
+
+    /// VARIABLES
+    char buffer[256];
+    C_Info x;
+    std::vector<uint8_t> msg;
+    int msg_size;
+    ara::com::Deserializer dser;
+
+    // We take the sever port number as the first argument
+    cout << "\t[SERVER] mypid: " << getpid() << endl;
+    /* TCP with client */
+    server_main_socket.OpenSocket(SERVER_PORT);
+    server_main_socket.BindServer();
+    server_main_socket.ListenServer(MAX_QUEUE_CLIENTS);
+
+    server_skeleton_ptr->OfferService();
+
+    cout << "shared " << server_skeleton_ptr << endl;
+    cout << "\t[SERVER]  Ready" << endl;
+    Socket Sclient = server_main_socket.AcceptServer();
+    cout << "\t[SERVER]  accepted" << endl;
+
+    strcpy(buffer, "=> Server connected...\n");
+
+    // send a confirmation connect to client
+    Sclient.Send(buffer, strlen(buffer));
+
+    // Receive a struct from client containing the method name and parameters
+    Sclient.Receive((void *)&msg_size, sizeof(msg_size));
+    msg.resize(msg_size);
+    Sclient.Receive((void *)&msg[0], sizeof(msg));
+
+    // Sclient.Receive((void *)&x, sizeof(x));
+
+    // print the requested method
+    // cout << blue << "\t[SERVER] " << x.method_name << endl;
+
+    // Perform the requested method then send the result
+    server_skeleton_ptr->method_dispatch(msg, Sclient);
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    while (server_skeleton_ptr->event1.getsub().empty())
+    {
+    }
+    std::cout << "\t[SERVER] : ";
+    server_skeleton_ptr->event1.update(7);
+
+    sleep(1);
+    while (server_skeleton_ptr->event2.getsub().empty())
+    {
+    }
+    std::cout << "\t[SERVER] : ";
+
+    server_skeleton_ptr->event2.update(9);
+
+    int y = 2;
+    while (server_skeleton_ptr->field1.getsub().empty())
+    {
+    }
+
+    std::cout << "\t[SERVER] : ";
+    server_skeleton_ptr->field1.update(y);
+
+    while (!server_skeleton_ptr->field1.getsub().empty()) // test uns
+    {
+    }
+
+    cout << "\n\t[SERVER] Goodbye..." << endl;
+
+    // always be awake to receive stuff from signal
+    while (1)
+    {
+        printf(".\n");
+        sleep(1);
+    }
+
+    server_main_socket.CloseSocket();
+    server_skeleton_ptr->StopOfferService();
+}
+
+void *pthread1(void *v_var)
+{
+    server_main_socket_DG.OpenSocket(SERVER_PORT);
+    server_main_socket_DG.BindServer();
+    cout << "shared " << server_skeleton_ptr << endl;
+    while (1)
+        Handle_IO();
+}
+
 int main()
 {
     server_skeleton_ptr = (skeleton *)mmap(NULL, sizeof *server_skeleton_ptr, PROT_READ | PROT_WRITE,
                                            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     memcpy(server_skeleton_ptr, &server_skeleton_obj, sizeof(server_skeleton_obj));
+    
 
-    int pid = fork();
-    if (pid == 0)
-    {
-        CServer server_main_socket(SOCK_STREAM);
+    pthread_t threads[NUM_THREADS];
 
-        /// VARIABLES
-        char buffer[256];
-        C_Info x;
-        std::vector<uint8_t> msg;
-        int msg_size;
-        ara::com::Deserializer dser;
+    int th1 , th2;
+    int i = 0;
 
-        // We take the sever port number as the first argument
-        cout << "\t[SERVER] mypid: " << getpid() << endl;
-        /* TCP with client */
-        server_main_socket.OpenSocket(SERVER_PORT);
-        server_main_socket.BindServer();
-        server_main_socket.ListenServer(MAX_QUEUE_CLIENTS);
-
-        server_skeleton_ptr->OfferService();
-
-        cout << "shared " << server_skeleton_ptr << endl;
-        cout << "\t[SERVER]  Ready" << endl;
-        Socket Sclient = server_main_socket.AcceptServer();
-        cout << "\t[SERVER]  accepted" << endl;
-
-        strcpy(buffer, "=> Server connected...\n");
-
-        // send a confirmation connect to client
-        Sclient.Send(buffer, strlen(buffer));
-
-        // Receive a struct from client containing the method name and parameters
-        Sclient.Receive((void *)&msg_size, sizeof(msg_size));
-        msg.resize(msg_size);
-        Sclient.Receive((void *)&msg[0], sizeof(msg));
-
-        // Sclient.Receive((void *)&x, sizeof(x));
-
-        // print the requested method
-        // cout << blue << "\t[SERVER] " << x.method_name << endl;
-
-        // Perform the requested method then send the result
-        server_skeleton_ptr->method_dispatch(msg, Sclient);
-
-        /////////////////////////////////////////////////////////////////////////////////////
-        while (server_skeleton_ptr->event1.getsub().empty())
+        th1 = pthread_create(&threads[0] , NULL , pthread0 , (void *)&i);
+        if(th1)
         {
-        }
-        std::cout << "\t[SERVER] : ";
-        server_skeleton_ptr->event1.update(7);
+            cout << "ERRor" << th1 << endl;
+            exit(-1);
 
-        sleep(1);
-        while (server_skeleton_ptr->event2.getsub().empty())
+        }
+        i = 1;
+        th2 = pthread_create(&threads[1] , NULL , pthread1 , (void *)&i);
+        if(th2)
         {
+            cout << "ERRor" << th2 << endl;
+            exit(-1);
+
         }
-        std::cout << "\t[SERVER] : ";
-
-        server_skeleton_ptr->event2.update(9);
-
-        int y = 2;
-        while (server_skeleton_ptr->field1.getsub().empty())
-        {
-        }
-
-        std::cout << "\t[SERVER] : ";
-        server_skeleton_ptr->field1.update(y);
-
-        while (!server_skeleton_ptr->field1.getsub().empty()) // test uns
-        {
-        }
-
-        cout << "\n\t[SERVER] Goodbye..." << endl;
-
-        // always be awake to receive stuff from signal
-        while (1)
-        {
-            printf(".\n");
-            sleep(1);
-        }
-
-        server_main_socket.CloseSocket();
-        server_skeleton_ptr->StopOfferService();
-    }
-    else
-    {
-        server_main_socket_DG.OpenSocket(SERVER_PORT);
-        server_main_socket_DG.BindServer();
-        cout << "shared " << server_skeleton_ptr << endl;
-        while (1)
-            Handle_IO();
-    }
-
+    
+    pthread_exit(NULL);
     return 0;
 }
 
@@ -158,7 +185,7 @@ void Handle_IO()
             std::cout << "got value of set\n";
         }
     }
-    printf("\n[SERVER]  ->> Handling client %s %d  with msg size %d\n", inet_ntoa(echoClntAddr.sin_addr), cudp.port,  evr.data_size);
+    printf("\n[SERVER]  ->> Handling client %s %d  with msg size %d\n", inet_ntoa(echoClntAddr.sin_addr), cudp.port, evr.data_size);
 
     switch (evr.service_id)
     {
@@ -172,7 +199,7 @@ void Handle_IO()
             break;
         case 1:
             std::cout << "[server] sub event2 start\n";
-            server_skeleton_ptr->event2.handlecall(evr, msg,cudp);
+            server_skeleton_ptr->event2.handlecall(evr, msg, cudp);
 
             break;
         case 2:
