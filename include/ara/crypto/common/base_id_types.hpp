319225982457507e5c1f5a32267508697f8f17bb
renad-mostafa99 <74263134+renad-mostafa99@users.noreply.github.com>
2022-03-18 06:22:34 +0200

    update base_id_type

--- include/ara/crypto/common/base_id_types.hpp
+++ include/ara/crypto/common/base_id_types.hpp
@@ -1,11 +1,24 @@
 #ifndef ARA_CRYPTO_BASE_ID_TYPES_H
 #define ARA_CRYPTO_BASE_ID_TYPES_H
-
+#include<string>
 namespace ara
 {
   namespace crypto
   {
-  
+    //SWS_CRYPT_10014
+    /**
+     * @brief Container type of the Crypto Algorithm Identifier.
+     * @file base_id_types.hpp
+     */
+    using CryptoAlgId = std::uint64_t;
+    //SWS_CRYPT_10015
+    /**
+     * @brief A container type and constant bit-flags of allowed usages of a key or a secret seed object. 
+     * Only directly specified usages of a key are allowed,
+     * all other are prohibited! Similar set of flags are defined for the usage restrictions of original key/seed and for 
+     * a symmetric key or seed that potentially can be derived from the original one.
+     */
+    using AllowedUsageFlags = std::uint32_t;
      //SWS_CRYPT_10016
      /**
       * defines all types of crypto objects (types of content that can be stored to a key slot)
