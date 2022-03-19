46be1c11ace9e2c5ccc19579e9d6eea30a223476
nehalyasser <64105565+nehalyasser@users.noreply.github.com>
2022-03-19 23:31:35 +0200

    Update CryptoContext.cpp

--- src/ara/crypto/cryp/CryptoContext.cpp
+++ src/ara/crypto/cryp/CryptoContext.cpp
@@ -1,23 +1,37 @@
 
-#include "ara/crypto/cryp/CryptoContext.hpp"
-#include "ara/crypto/cryp/cryobj/crypto_primitive_id.hpp"
+#include "ara/crypto/cryp/crypto_context.hpp"
+
 using namespace ara::crypto::cryp;
 
-class CryptoContext {
+/* Destructor */
+virtual CryptoContext:: ~CryptoContext() noexcept = default
+{
 
-	virtual ~CryptoContext() noexcept = default;
-	//Return CryptoPrimitivId instance containing instance identification.//
-	virtual CryptoPrimitiveId::Uptr GetCryptoPrimitiveId() const noexcept = 0;
-	//Check if the crypto context is already initialized and ready to use. It checks all required values//
-        //including: key value, IV / seed, etc...//
-	virtual bool IsInitialized() const noexcept = 0;
-	//Copy-assign another CryptoContext to this instance//
-	CryptoContext& operator= (const CryptoContext& other) = default;
-	//Move-assign another CryptoContext to this instance.//
-	CryptoContext& operator= (CryptoContext&& other) = default;
-	//Get a reference to Crypto Provider of this context.//
-	//virtual CryptoProvider& MyProvider() const noexcept = 0;
+}
+/*Return CryptoPrimitivId instance containing instance identification */
+virtual CryptoPrimitiveId::Uptr GetCryptoPrimitiveId() const noexcept = 0
+{
+
+}
+/* Check if the crypto context is already initializedand ready to use */
+virtual bool CryptoContext:: IsInitialized() const noexcept = 0
+{
+
+}
+/* Copy constructor */
+CryptoContext:: CryptoContext& operator= (const CryptoContext& other) = default
+{
+
+}
+/* Move constructor */
+CryptoContext:: CryptoContext& operator= (CryptoContext&& other) = default
+{
+
+}
+/* Get a reference to Crypto Provider of this context */
+virtual CryptoContext:: CryptoProvider& MyProvider() const noexcept = 0
+{
+
+}
 
 
-
-};
