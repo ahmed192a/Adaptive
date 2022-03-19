2f0ca8c8775cdc0aaa5879ee37fdff08caf3510a
Aly Sabie <alyysab3@gmail.com>
2022-03-19 22:41:22 +0200

    OTA: Editted the main function

--- App/OTA/ota_main.cpp
+++ App/OTA/ota_main.cpp
@@ -13,19 +13,19 @@ int main(void) {
 
     std::cout << std::endl << "[OTA] started ..." << std::endl << std::endl;
     
-    // The instance that will deal with storing and retieving the meta-data
-    MetaDataStorage fileSystem(META_DATA_FILE_PATH);
+    // // The instance that will deal with storing and retieving the meta-data
+    // MetaDataStorage fileSystem(META_DATA_FILE_PATH);
 
 
-    std::string json = """{\"appID\": \"K134\",\"appName\":\"OTA_APP\",\"sizeInBytes\":137,\"platformName\":\"adaptive\",\"version\": \"1.0.4\",\"result\":\"ok\"}""";
-    // communication with the cloud to get the meta-data in the form of json string
+    // std::string json = """{\"appID\": \"K134\",\"appName\":\"OTA_APP\",\"sizeInBytes\":137,\"platformName\":\"adaptive\",\"version\": \"1.0.4\",\"result\":\"ok\"}""";
+    // // communication with the cloud to get the meta-data in the form of json string
 
 
-    MetaData metaData(json);
+    // MetaData metaData(json);
 
-    // after some logical proccessing 
+    // // after some logical proccessing 
 
-    fileSystem.save_MetaData(metaData);
+    // fileSystem.save_MetaData(metaData);
 
 
     
