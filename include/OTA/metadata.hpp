b2786e76fc79acf22d686049108e3aa4b29a0356
Ahmed Osama Mohamed <42383306+ahmedosamataha@users.noreply.github.com>
2022-03-20 00:09:17 +0200

    Update metadata.hpp

--- include/OTA/metadata.hpp
+++ include/OTA/metadata.hpp
@@ -7,30 +7,30 @@
 class MetaData
 {
     //MetaData variables
-    string platformName;
-    string appName;
-    string appID;
+    std::string platformName;
+    std::string appName;
+    std::string appID;
     UpdateVersion v;
     unsigned long sizeInBytes;
 
 public:
     // Constructor Overloading to support different types of initializations
     MetaData();
-    MetaData(string platformName, string appName, string appID, unsigned long sizeInBytes, string ver);
-    MetaData(string jsonStr);
+    MetaData(std::string platformName, std::string appName, std::string appID, unsigned long sizeInBytes, std::string ver);
+    MetaData(std::string jsonStr);
 
     //operator overloading for ">" sign
     bool operator > (MetaData Meta2);
 
     // setters and getters for the class members
-    void set_platformName(string platformName);
-    string get_platformName(void);
+    void set_platformName(std::string platformName);
+    std::string get_platformName(void);
 
-    void set_appName(string appName);
-    string get_appName(void);
+    void set_appName(std::string appName);
+    std::string get_appName(void);
 
-    void set_appID(string appID);
-    string get_appID(void);
+    void set_appID(std::string appID);
+    std::string get_appID(void);
 
     void set_sizeInBytes(unsigned long sizeInBytes);
     unsigned long get_sizeInBytes(void);
