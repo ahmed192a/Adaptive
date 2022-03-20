0afa17426afed288482427d73cede5958cdef009
KhaledNagah <54414359+KhaledNagah@users.noreply.github.com>
2022-03-20 22:11:58 +0200

    Add files via upload

--- include/OTA/client.hpp
+++ include/OTA/client.hpp
@@ -1,36 +1,94 @@
-#ifndef CLIENT_H
-#define CLIENT_H
+#ifndef OTA_CLIENT_H
+#define OTA_CLIENT_H
 
-#include <iostream> //cout
-#include <string.h> //strlen
-#include <sys/socket.h> //socket
+#include <iostream> //string
 #include <arpa/inet.h> //inet_addr
-#include <unistd.h>// close socket
 
-#define IP_CLOUD "127.0.0.1"
-#define PORT_CLOUD 8888
-#define BUFFER_SIZE 100000
+#define OTA_IP_CLOUD "127.0.0.1"
+#define OTA_PORT_CLOUD 8888
+#define OTA_PACKAGE_BUFFER_SIZE 10000
+#define OTA_METADATA_BUFFER_SIZE 1000
 
-using namespace std;
+namespace OTA{
 
 class Client
 {
 private:
         int sock;
         struct sockaddr_in server;
-        string address;
+        std::string address;
         int port;
 
-        bool sendData(string data);
+        bool sendData(std::string data);
 
 
     public:
+
         Client();
-        
-        bool cloudConnect(string address, int port);
-        bool requestMetadata(char * data);
+    /**
+     *  @brief Connects the OTA application to the cloud server.
+     *
+     *  @details
+     *   Connects the OTA application to the cloud server which
+     *   has the update package and its metadata. 
+     * 
+     *  @param address  IPV4 address of the cloud server.
+     *  @param port The port number to connect to on the cloud server.
+     *  @return     Returns zero in case failed to connect to the cloud server.
+     */
+
+        bool cloudConnect(std::string address, int port);
+
+    
+    
+    
+    /**
+     *  @brief Requests the package metadata from the cloud server.
+     *
+     *  @details
+     *   Requests the package metadata from the cloud server. 
+     *   The package metadata has information like appID, appName,... 
+     * 
+     *  @param data  A pointer to characters to store the requested metadata in.
+     *  @return     Returns zero in case failed to get the package metadata from the cloud server.
+
+     */
+
+        bool requestMetadata(std::string  * data);
+
+    
+    
+    
+    /**
+     *  @brief Requests the package from the cloud server.
+     *
+     *  @details
+     *   Requests the package from the cloud server. 
+     *   The package is the update itself
+     * 
+     *  @param data  A pointer to characters to store the requested package in.
+     *  @return     Returns zero in case failed to get the package from the cloud server.
+
+     */
+
         bool requestPackage(char * data);
+
+
+
+    /**
+     *  @brief Disconnects the OTA application to the cloud server.
+     *
+     *  @details
+     *   Disconnects the OTA application to the cloud server.
+     * 
+     *  @param   
+     *  @return     
+
+     */
         void cloudDisconnect();
 };
+
+}
+
 
 #endif
