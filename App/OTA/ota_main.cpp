2d836b621aea3ccc17b720498c0d093e860f083b
Aly Sabie <alyysab3@gmail.com>
2022-03-20 21:33:31 +0200

    ota editted

--- App/OTA/ota_main.cpp
+++ App/OTA/ota_main.cpp
@@ -5,37 +5,53 @@
  ********************************************************************************/
 
 #include <iostream>
+#include <string>
 #include "OTA/meta_data_storage.hpp"
 #include "OTA/client.hpp"
 
-#define META_DATA_FILE_PATH "./testFile.dat"
+#define META_DATA_FILE_PATH "./meta-data.dat"
+
+using namespace ota;
 
 int main(void) {
 
-    std::cout << std::endl << "[OTA] started ..." << std::endl << std::endl;
+    std::cout << std::endl << "[OTA] started ..." << std::endl;
 
-    // creating the cloud client
-    std::cout << std::endl << "[OTA] connecting to the cloud server ..." << std::endl << std::endl;
+    // creating the cloud client and connecting to the server
+    std::cout << "[OTA] connecting to the cloud server ..." << std::endl;
     Client cloud;
-    while( !cloud.cloudConnect(IP_CLOUD, PORT_CLOUD) ) {
-        std::cout << std::endl << "[OTA] failed to connect ..." << std::endl << std::endl;
-        sleep(1);
+    
+    if( cloud.cloudConnect(IP_CLOUD, PORT_CLOUD) ) {
+        std::cout << "[OTA] connection succeeded ..." << std::endl;
+    }
+    else {
+        std::cout << "[OTA] connection failed..." << std::endl;
     }
 
+    // requesting the metaData
+    char array[BUFFER_SIZE];
+    cloud.requestMetadata(array);
+    std::string json = array;
+
+
+    MetaData metaData( json );
+
+
+    std::cout << metaData << std::endl;
     
-    // The instance that will deal with storing and retieving the meta-data
-    MetaDataStorage fileSystem(META_DATA_FILE_PATH);
+    // // The instance that will deal with storing and retieving the meta-data
+    // MetaDataStorage fileSystem(META_DATA_FILE_PATH);
 
 
-    std::string json = """{\"appID\": \"K134\",\"appName\":\"OTA_APP\",\"sizeInBytes\":137,\"platformName\":\"adaptive\",\"version\": \"1.0.4\",\"result\":\"ok\"}""";
-    // communication with the cloud to get the meta-data in the form of json string
+    // // std::string json = """{\"appID\": \"K134\",\"appName\":\"OTA_APP\",\"sizeInBytes\":137,\"platformName\":\"adaptive\",\"version\": \"1.0.4\",\"result\":\"ok\"}""";
+    // // communication with the cloud to get the meta-data in the form of json string
 
 
-    MetaData metaData(json);
+    // MetaData metaData( std::string(json) );
 
-    // after some logical proccessing 
+    // // after some logical proccessing 
 
-    fileSystem.save_MetaData(metaData);
+    // fileSystem.save_MetaData(metaData);
 
 
     
