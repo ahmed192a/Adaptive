9dac7046d170df08cbffe0db6f9b6d23094ba955
Aly Sabie <alyysab3@gmail.com>
2022-03-20 20:10:38 +0200

    Editted the OTA application

--- App/OTA/ota_main.cpp
+++ App/OTA/ota_main.cpp
@@ -6,12 +6,22 @@
 
 #include <iostream>
 #include "OTA/meta_data_storage.hpp"
+#include "OTA/client.hpp"
 
 #define META_DATA_FILE_PATH "./testFile.dat"
 
 int main(void) {
 
     std::cout << std::endl << "[OTA] started ..." << std::endl << std::endl;
+
+    // creating the cloud client
+    std::cout << std::endl << "[OTA] connecting to the cloud server ..." << std::endl << std::endl;
+    Client cloud;
+    while( !cloud.cloudConnect(IP_CLOUD, PORT_CLOUD) ) {
+        std::cout << std::endl << "[OTA] failed to connect ..." << std::endl << std::endl;
+        sleep(1);
+    }
+
     
     // The instance that will deal with storing and retieving the meta-data
     MetaDataStorage fileSystem(META_DATA_FILE_PATH);
