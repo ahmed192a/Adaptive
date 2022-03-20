937dd956efbd0a21b806cb9c1bc981d30776d877
Ali Abdelhafez <42843726+aliabdelhafez@users.noreply.github.com>
2022-03-20 17:37:09 +0200

    Update vendor_specific_algorithm_identifier.hpp

--- include/ara/crypto/common/vendor_specific_algorithm_identifier.hpp
+++ include/ara/crypto/common/vendor_specific_algorithm_identifier.hpp
@@ -3,16 +3,11 @@
 #define ARA_CRYPTO_COMMON_VENDOR_SPECIFIC_ALGORITHM_IDENTIFIER_HPP
 
 #include <string>
+#define ALGORITHMSNUMBER 1
 
-//enter the algorithm name used according to the standard naming and two times
-//in enum and array of string
-
-//HMACSHA_256 is here for example and testing
-enum algorithmsId
-	{
-	HMACSHA_256=1
-
-	 } ;
+//enter the algorithm name used according to the standard naming in the array of string
+//the algorithm ID will be shifted by one less when trying to access the array
+//then AlgId of HMACSHA_256 is 1 and when used in algorithmNames variable will be 0
 const std::string algorithmNames[] =
 {
 	"HMACSHA_256"
