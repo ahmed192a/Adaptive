fb0a715c6295821e2ee75f3c5b261c081e2277d5
Ali Abdelhafez <42843726+aliabdelhafez@users.noreply.github.com>
2022-03-23 12:52:15 +0200

    Update crypto_provider.hpp

--- include/ara/crypto/cryp/crypto_provider.hpp
+++ include/ara/crypto/cryp/crypto_provider.hpp
@@ -2,7 +2,9 @@
 #define CRYPTO_PROVIDER_H_
 
 #include "ara/crypto/common/volatile_trusted_container.hpp"
-#include "ara/crypto/cryp/message_authn_code_ctx.hpp"
+#include "ara/crypto/cryp/HMAC.hpp"
+#include "../common/vendor_specific_algorithm_identifier.hpp"
+//#include "ara/crypto/cryp/message_authn_code_ctx.hpp"
 //#include "ara/crypto/cryp/symmetric_key_wrapper_ctx.hpp"
 //#include "ara/crypto/cryp/key_derivation_function_ctx.hpp"
 //#include "ara/crypto/cryp/auth_cipher_ctx.hpp"
