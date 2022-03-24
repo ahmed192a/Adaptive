e358521289f85ef297c0542cae5cddbf5a70d449
nehalyasser <nehalyasser1999@gmail.com>
2022-03-24 22:34:05 +0200

    Update authentication.cpp

--- src/ara/crypto/cryp/authentication.cpp
+++ src/ara/crypto/cryp/authentication.cpp
@@ -2,7 +2,7 @@
 
 using namespace ara::crypto::cryp;
 
-//@brief: a constructor saves the instance of the crypto provider of the Authentication context
+///@brief: a constructor saves the instance of the crypto provider of the Authentication context
 Authentication::Authentication(CryptoProvider* myProvider)
 {
 	//@ stores the crypto provider instance of the context in the member "myCryptoProvider" inherited from auth_cipher_ctx
@@ -16,7 +16,7 @@ Authentication::Authentication(CryptoProvider* myProvider)
 				/***** implementation of inherited CyrptoContext virtual functions *****/
 				/***********************************************************************/
 
-//@brief: inherited function from CryptoContext, determines whether context is ready to use or not 
+///@brief: inherited function from CryptoContext, determines whether context is ready to use or not 
 bool Authentication :: IsInitialized()
 {
 	//@ return false if the context is not initialized
@@ -31,13 +31,13 @@ bool Authentication :: IsInitialized()
 	}
 }
 
-//@brief: inherited function from CryptoContext, gets a reference to CryptoPrimitivId instance of this CryptoContext
+///@brief: inherited function from CryptoContext, gets a reference to CryptoPrimitivId instance of this CryptoContext
 CryptoPrimitiveId::Uptr Authentication:: GetCryptoPrimitiveId() const noexcept
 {
 
 }
 
-//@brief: inherited function from CryptoContext, gets a reference to Crypto Provider instance of this CryptoContext
+///@brief: inherited function from CryptoContext, gets a reference to Crypto Provider instance of this CryptoContext
 CryptoProvider&  Authentication:: MyProvider() const noexcept
 {
 	//@return pointer references the Crypto Provider instance of the context
@@ -45,6 +45,49 @@ CryptoProvider&  Authentication:: MyProvider() const noexcept
 	return *myCryptoProvider;
 }
                 /*************************************************************************/
-
                 /***** implementation of auth_cipher_ctx inherited virtual functions *****/
                 /*************************************************************************/
+
+///@brief: Compare the calculated digest from MAC against an expected signature object
+bool Authentication:: Check(const Signature& expected) const noexcept
+{
+	// call the function Check of the mac context of this authentication instance and return a bool that is the same value
+	//returned from Check of the mac context
+	return this->macPtr->Check(expected);
+}
+
+///@brief: Initialize the mac context of this authentication instance for a new data processing 
+void Authentication:: Start(ReadOnlyMemRegion iv = ReadOnlyMemRegion()) noexcept
+{
+
+}
+
+///@brief: Initialize the mac context of this authentication instance  for a new data processing
+void Authentication::Start(const SecretSeed& iv) noexcept
+{
+
+}
+
+///@brief: Update the digest calculation by the specified RestrictedUseObject.
+void Authentication::UpdateAssociatedData(const RestrictedUseObject& in) noexcept
+{
+	this->macPtr->Update(in);
+}
+
+///@brief: Update the digest calculation by a new chunk of associated data
+void Authentication::UpdateAssociatedData(ReadOnlyMemRegion in) noexcept
+{
+	this->macPtr->Update(in);
+}
+
+///@brief: Update the digest calculation by the specified Byte
+void Authentication:: UpdateAssociatedData(std::uint8_t in) noexcept
+{
+	this->macPtr->Update(in);
+}
+
+/// @brief: resets the context
+void Authentication:: Reset() noexcept
+{
+
+}
