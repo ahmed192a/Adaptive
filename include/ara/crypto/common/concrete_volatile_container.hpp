2959132e375d8f66192d57e70810e52042257f51
Ali Abdelhafez <42843726+aliabdelhafez@users.noreply.github.com>
2022-04-22 11:54:22 +0200

    Add files via upload

---
+++ include/ara/crypto/common/concrete_volatile_container.hpp
@@ -0,0 +1,29 @@
+#ifndef ARA_CRYPTO_CONCRETE_VOLATILE_CONTAINER_HPP_
+#define ARA_CRYPTO_CONCRETE_VOLATILE_CONTAINER_HPP_
+#include "volatile_trusted_container.hpp"
+#include "concrete_io_interface.hpp"
+
+namespace ara
+{
+    namespace crypto
+	{
+        class ConcreteVolatileTrustedContainer : public VolatileTrustedContainer
+		{
+            
+		public:
+        static ConcreteIOInterface myinterface;
+            std::vector<uint8_t> buffer;
+            ConcreteVolatileTrustedContainer(std::size_t capacity,IOInterface& instance);
+
+             IOInterface& GetIOInterface () const noexcept=0;
+
+			 ~ConcreteVolatileTrustedContainer () noexcept=default;
+
+
+        };
+    }
+}
+
+
+
+#endif
