acc1f6b33fc63b6c801a4d8321fd8ae949311011
Ahmed Osama Mohamed <42383306+ahmedosamataha@users.noreply.github.com>
2022-04-23 02:29:13 +0200

    Update hash_service.hpp

--- include/ara/crypto/cryp/hash_service.hpp
+++ include/ara/crypto/cryp/hash_service.hpp
@@ -8,7 +8,7 @@
 #ifndef ARA_CRYPTO_HASH_SERVICE_H
 #define ARA_CRYPTO_HASH_SERVICE_H
 
-#include "hash_function_ctx.hpp"
+#include "ara/crypto/cryp/hash_function_ctx.hpp"
 
 
 namespace ara {
@@ -18,23 +18,26 @@ namespace ara {
             class HashService : public HashFunctionCtx
             {
             private:
-                /* data */
+                std::vector<std::byte> hashedValue; 
+                std::vector<std::byte> vectorToBeHashed;
+                std::string x;
+                bool flagInit = 0;
+                CryptoProvider * myProvider;
+
+
             public:
 
                 /// @brief Unique smart pointer of HashService.
                 using Uptr = std::unique_ptr<HashService>;
 
+                HashService(CryptoProvider * myProvider);
+
 
                 /************************* HASHFUNCTIONCTX METHODS ***********************************/
-                /// @brief Create Hash Function Context as it implement hashing and store calculated hash
-                /// @param[in] algID identifier of the target crypto algorithm
-                /// @returns unique smart pointer to the created context
-                ara::core::Result<HashFunctionCtx::Uptr> CreateHashFunctionCtx(AlgId algId) noexcept=0;
-
 
                 /// @brief Finish the digest calculation and optionally produce the "signature" object. Only after call of this method the digest can be signed, verified, extracted or compared.
                 /// @returns unique smart pointer to created signature object,
-                ara::core::Result<ara::core::Vector<ara::core::Byte> > Finish() noexcept=0;
+                std::vector<std::byte> Finish() noexcept=0;
 
 
                 /// @brief Get DigestService instance.
@@ -45,19 +48,19 @@ namespace ara {
                 /// @brief Get requested part of calculated digest.
                 /// @param[in] offset position of the first byte of digest that should be placed to the output buffer.
                 /// @returns number of digest bytes really stored to the output buffer
-                ara::core::Result<ara::core::Vector<ara::core::Byte>> GetDigest (std::size_t offset=0) const noexcept=0;
+                std::vector<std::byte> GetDigest (std::size_t offset=0) const noexcept=0;
 
                 /// @brief Initialize the context for a new data stream processing or generation (depending on the primitive) without IV.
                 /// @returns ara::core::Result<void>
-                ara::core::Result<void> Start() noexcept=0;
+                void Start() noexcept=0;
 
                 /// @brief Update the digest calculation context by a new part of the message. This method is dedicated for cases then the RestrictedUseObject is a part of the "message".
                 /// @param[in] in part of input message processed (byte value to br processed)
-                ara::core::Result<void> Update (std::uint8_t in) noexcept=0;
+                void Update (std::uint8_t in) noexcept=0;
 
                 /// @brief Update the digest calculation context by a new part of the message. This method is dedicated for cases then the RestrictedUseObject is a part of the "message".
                 /// @param[in] in part of input message processed 
-                ara::core::Result<void> Update (ReadOnlyMemRegion in) noexcept=0;
+                void Update (ReadOnlyMemRegion in) noexcept=0;
 
 
                 /****************************** CRYPTOCONTEXT METHODS ***************************/
@@ -71,7 +74,7 @@ namespace ara {
 
 
 
-                HashService(/* args */);
+                
                 ~HashService();
             };
             
