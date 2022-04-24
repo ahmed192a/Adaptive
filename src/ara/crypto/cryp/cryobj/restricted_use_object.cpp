21858bcb9787b960dba9dcb165ca6c6d73e2c974
renad-mostafa99 <74263134+renad-mostafa99@users.noreply.github.com>
2022-04-24 17:05:44 +0200

    Create restricted_use_object.cpp

---
+++ src/ara/crypto/cryp/cryobj/restricted_use_object.cpp
@@ -0,0 +1,16 @@
+#include <ara\crypto\cryp\cryobj\rest_use_obj.hpp>
+
+#include <iostream>
+namespace ara
+{
+    namespace crypto
+    {
+        namespace cryp
+        {
+            RestrictedUseObject::Usage RestUseObj::GetAllowedUsage () const 
+            {
+                return this->Allow_U;
+            }
+        }
+    }
+}
