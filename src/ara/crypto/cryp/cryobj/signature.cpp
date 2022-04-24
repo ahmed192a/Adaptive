a35ea661596451aad43d9ec02a3165c607440bd6
Rawan Khaled <67297151+rere-kh1999@users.noreply.github.com>
2022-04-24 22:50:46 +0200

    Update signature.cpp

--- src/ara/crypto/cryp/cryobj/signature.cpp
+++ src/ara/crypto/cryp/cryobj/signature.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "ara/crypto/cryp/cryobj/sign.hpp"
 
+namespace ara
+{
+    namespace crypto
+    {
+        namespace cryp
+        {
+            CryptoPrimitiveId::AlgId Sign:: GetHashAlgId () const
+            {
+                return this->H_SingID ;
+            }
+            std::size_t Sign:: GetRequiredHashSize () const 
+            {
+                return this->H_Req_Size;
+            }
+        }
+    }
+}
