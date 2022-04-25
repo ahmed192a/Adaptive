bfeea4d336bf9a2b35abfffc9fcd6cf882fad17a
Basmala <59097918+BasmalaMagdy@users.noreply.github.com>
2022-04-25 22:39:08 +0200

    Update service_skeleton.hpp

--- include/ara/com/proxy_skeleton/skeleton/service_skeleton.hpp
+++ include/ara/com/proxy_skeleton/skeleton/service_skeleton.hpp
@@ -41,10 +41,10 @@ namespace ara
                 public:
                     struct SK_Handle
                     {
-                       
+
                         uint32_t service_portnum;
                         uint32_t sd_portnum;
-                         //CClient *m_server_UPD;
+                        // CClient *m_server_UPD;
                     };
                     SK_Handle m_skeleton_handle;
                     ara::com::InstanceIdentifier &m_service_id;
@@ -55,15 +55,14 @@ namespace ara
                         InstanceIdentifier &instance,
                         SK_Handle skeleton_handle,
                         MethodCallProcessingMode mode = MethodCallProcessingMode::kEvent)
-                            : m_service_id(service_id),
-                            m_skeleton_udp(SOCK_DGRAM),
-                            m_instance_id(instance),
-                            m_mode{mode},
-                            m_skeleton_handle{skeleton_handle}
+                        : m_service_id(service_id),
+                          m_skeleton_udp(SOCK_DGRAM),
+                          m_instance_id(instance),
+                          m_mode{mode},
+                          m_skeleton_handle{skeleton_handle}
                     {
-                        
 
-                       // skeleton_handle.m_server_UPD = &m_skeleton_udp;
+                        // skeleton_handle.m_server_UPD = &m_skeleton_udp;
                         this->cliaddr.sin_family = AF_INET; // IPv4
                         this->cliaddr.sin_addr.s_addr = INADDR_ANY;
                         this->cliaddr.sin_port = htons(m_skeleton_handle.sd_portnum);
@@ -71,48 +70,48 @@ namespace ara
 
                     virtual ~ServiceSkeleton() {}
 
-                    void OfferService() 
+                    void OfferService()
                     {
                         this->m_skeleton_udp.OpenSocket();
-                        const uint32_t minorV= 0;
-                        const uint8_t majorV= 1;
+                        const uint32_t minorV = 0;
+                        const uint8_t majorV = 1;
                         SOMEIP_MESSAGE::sd::SomeIpSDMessage sd_msg;
-                        entry::ServiceEntry offer_service_e = entry::ServiceEntry::CreateOfferServiceEntry (m_service_id.GetInstanceId(),0, majorV, minorV);
-                        option::Ipv4EndpointOption optionA=option::Ipv4EndpointOption::CreateSdEndpoint(false ,helper::Ipv4Address(127, 0, 0, 1) ,option::Layer4ProtocolType::Udp,m_skeleton_handle.service_portnum);
+                        entry::ServiceEntry offer_service_e = entry::ServiceEntry::CreateOfferServiceEntry(m_service_id.GetInstanceId(), 0, majorV, minorV);
+                        option::Ipv4EndpointOption optionA = option::Ipv4EndpointOption::CreateSdEndpoint(false, helper::Ipv4Address(127, 0, 0, 1), option::Layer4ProtocolType::Udp, m_skeleton_handle.service_portnum);
                         offer_service_e.AddFirstOption(&optionA);
                         sd_msg.AddEntry(&offer_service_e);
                         std::vector<uint8_t> _payload = sd_msg.Serializer();
-                        uint32_t _size=_payload.size();
-                        this->m_skeleton_udp.UDPSendTo((  void *)&_size, sizeof(_size), ( struct sockaddr *) &this->cliaddr);
-                        this->m_skeleton_udp.UDPSendTo((  void *)_payload.data(), _payload.size(), ( struct sockaddr *) &this->cliaddr);   
+                        uint32_t _size = _payload.size();
+                        this->m_skeleton_udp.UDPSendTo((void *)&_size, sizeof(_size), (struct sockaddr *)&this->cliaddr);
+                        this->m_skeleton_udp.UDPSendTo((void *)_payload.data(), _payload.size(), (struct sockaddr *)&this->cliaddr);
                         this->m_skeleton_udp.CloseSocket();
                     }
 
-                    void StopOfferService() 
+                    void StopOfferService()
                     {
-                        const uint32_t minorV= 0;
-                        const uint8_t majorV= 1;
+                        const uint32_t minorV = 0;
+                        const uint8_t majorV = 1;
                         const uint32_t stop_ttl = 0;
                         this->m_skeleton_udp.OpenSocket();
                         // SD_data service = {m_service_id.GetInstanceId(), getpid() ,m_skeleton_handle.service_portnum, false};
                         SOMEIP_MESSAGE::sd::SomeIpSDMessage sd_msg;
-                        entry::ServiceEntry stop_offer_e = entry::ServiceEntry::CreateOfferServiceEntry (m_service_id.GetInstanceId(),0, majorV, minorV,stop_ttl);
+                        entry::ServiceEntry stop_offer_e = entry::ServiceEntry::CreateOfferServiceEntry(m_service_id.GetInstanceId(), 0, majorV, minorV, stop_ttl);
                         sd_msg.AddEntry(&stop_offer_e);
                         std::vector<uint8_t> _payload = sd_msg.Serializer();
                         uint32_t _size = _payload.size();
-                        this->m_skeleton_udp.UDPSendTo((  void *)&_size, sizeof(_size), ( struct sockaddr *) &this->cliaddr);
-                        this->m_skeleton_udp.UDPSendTo((  void *)_payload.data(), _payload.size(), ( struct sockaddr *) &this->cliaddr);
+                        this->m_skeleton_udp.UDPSendTo((void *)&_size, sizeof(_size), (struct sockaddr *)&this->cliaddr);
+                        this->m_skeleton_udp.UDPSendTo((void *)_payload.data(), _payload.size(), (struct sockaddr *)&this->cliaddr);
                         this->m_skeleton_udp.CloseSocket();
                     }
 
                     /**
-                     * @brief 
-                     * @todo edit vector data in event info 
-                     * @tparam T 
-                     * @param event_id 
-                     * @param data 
-                     * @param is_field 
-                     * @param client_add 
+                     * @brief
+                     * @todo edit vector data in event info
+                     * @tparam T
+                     * @param event_id
+                     * @param data
+                     * @param is_field
+                     * @param client_add
                      */
                     template <typename T>
                     void SendEvent(int event_id, const T &data, sockaddr_in client_add)
@@ -128,16 +127,15 @@ namespace ara
                         msg_.operation = 0;
                         msg_.data_size = sermsg.size();
                         /* send the event info object then send the serialized data */
-                        std::cout<<"send event to client "<< client_add.sin_port<<std::endl;
+                        std::cout << "send event to client " << client_add.sin_port << std::endl;
                         // Create Mesaage {, event_id | 0x8000}, NOTIFICATION
-                        ara::com::SOMEIP_MESSAGE::Message M1({ (uint16_t) this->m_service_id.GetInstanceId(),(uint16_t) (event_id|0x8000)},{1,2},3,4,ara::com::SOMEIP_MESSAGE::MessageType::NOTIFICATION);
+                        ara::com::SOMEIP_MESSAGE::Message M1({(uint16_t)this->m_service_id.GetInstanceId(), (uint16_t)(event_id | 0x8000)}, {1, 2}, 3, 4, ara::com::SOMEIP_MESSAGE::MessageType::NOTIFICATION);
                         // SetPayload(sermsg)
                         M1.SetPayload(sermsg);
                         // vector jjj = mes.Serializer
-                        std::vector<uint8_t> __payload= M1.Serializer();
+                        std::vector<uint8_t> __payload = M1.Serializer();
                         // size of vector
                         uint32_t size = __payload.size();
-        
 
                         this->m_skeleton_udp.OpenSocket();
                         this->m_skeleton_udp.UDPSendTo((void *)&size, sizeof(size), (sockaddr *)&client_add);
@@ -146,17 +144,17 @@ namespace ara
                     }
 
                 protected:
-                /*1 R ARGS*/
+                    /*1 R ARGS*/
                     template <typename Class, typename R, typename... Args>
                     void HandleCall(Class &c,
                                     std::future<R> (Class::*method)(Args...),
                                     SOMEIP_MESSAGE::Message msg,
                                     Socket &binding)
                     {
-                        
+
                         sHandleCall(c, method, msg, binding, std::index_sequence_for<Args...>());
                     }
-                /*2 future<void> ARGS*/
+                    /*2 future<void> ARGS*/
                     template <typename Class, typename... Args>
                     void HandleCall(Class &c,
                                     std::future<void> (Class::*method)(Args...),
@@ -165,7 +163,7 @@ namespace ara
                     {
                         sHandleCall(c, method, msg, binding, std::index_sequence_for<Args...>());
                     }
-                /*3 R */
+                    /*3 R */
                     template <typename Class, typename R>
                     void HandleCall(Class &c,
                                     std::future<R> (Class::*method)(),
@@ -176,11 +174,11 @@ namespace ara
                         // create serialize object
                         ara::com::Serializer s1;
                         // serialize the result
-                         s1.serialize(result);
+                        s1.serialize(result);
                         // create SOME IP message
                         SOMEIP_MESSAGE::Message R_msg(
-                            SOMEIP_MESSAGE::Message_ID{ (uint16_t)this->m_service_id.GetInstanceId(), (uint16_t)(msg.MessageId().method_id&0x7fff) },
-                            SOMEIP_MESSAGE::Request_ID{5,6},
+                            SOMEIP_MESSAGE::Message_ID{(uint16_t)this->m_service_id.GetInstanceId(), (uint16_t)(msg.MessageId().method_id & 0x7fff)},
+                            SOMEIP_MESSAGE::Request_ID{5, 6},
                             2, // protocol version
                             7, // Interface Version
                             SOMEIP_MESSAGE::MessageType::RESPONSE);
@@ -192,10 +190,10 @@ namespace ara
                         // send message
                         binding.Send(&msg_size, sizeof(msg_size));
                         binding.Send(_payload.data(), msg_size);
-                        
+
                         binding.CloseSocket();
                     }
-                /*4 future <void>*/
+                    /*4 future <void>*/
                     template <typename Class>
                     void HandleCall(Class &c,
                                     std::future<void> (Class::*method)(),
@@ -203,11 +201,11 @@ namespace ara
                                     Socket &binding)
                     {
                         SOMEIP_MESSAGE::Message R_msg(
-                                SOMEIP_MESSAGE::Message_ID{ this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
-                                SOMEIP_MESSAGE::Request_ID{5,6},
-                                2, // protocol version
-                                7, // Interface Version
-                                SOMEIP_MESSAGE::MessageType::RESPONSE);
+                            SOMEIP_MESSAGE::Message_ID{this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
+                            SOMEIP_MESSAGE::Request_ID{5, 6},
+                            2, // protocol version
+                            7, // Interface Version
+                            SOMEIP_MESSAGE::MessageType::RESPONSE);
                         std::future<void> f = (c.*method)();
                         f.get();
                         std::vector<uint8_t> _payload = R_msg.Serializer();
@@ -216,7 +214,7 @@ namespace ara
                         binding.Send(_payload.data(), msg_size);
                         binding.CloseSocket();
                     }
-                /*5 void ARGS*/
+                    /*5 void ARGS*/
                     template <typename Class, typename... Args>
                     void HandleCall(Class &c,
                                     void (Class::*method)(Args...),
@@ -225,7 +223,7 @@ namespace ara
                     {
                         sHandleCall(c, method, msg, binding, std::index_sequence_for<Args...>());
                     }
-                /*6 void */
+                    /*6 void */
                     template <typename Class>
                     void HandleCall(Class &c,
                                     void (Class::*method)(),
@@ -233,47 +231,41 @@ namespace ara
                                     Socket &binding)
                     {
                         (c.*method)();
+                        binding.CloseSocket();
                     }
 
-                    template <typename T>
-                    std::future<T> NoHandler()
+                    void NoHandler(uint16_t method_id, Socket &binding)
                     {
-                        std::promise<T> p;
-                        try
-                        {
-                            // code that may throw
-                            throw std::runtime_error("Field No Handler");
-                        }
-                        catch (...)
-                        {
-                            try
-                            {
-                                // store anything thrown in the promise
-                                p.set_exception(std::current_exception());
-                            }
-                            catch (...)
-                            {
-                            } // set_exception() may throw too
-                        }
+                        SOMEIP_MESSAGE::Message R_msg(
+                            SOMEIP_MESSAGE::Request_ID{5, 6},
+                            SOMEIP_MESSAGE::Message_ID{this->m_service_id.GetInstanceId(), method_id},
+                            2, // protocol version
+                            7, // Interface Version
+                            SOMEIP_MESSAGE::ReturnCode::E_UNKNOWN_METHOD,
+                            SOMEIP_MESSAGE::MessageType::ERROR);
 
-                        return p.get_future();
+                        std::vector<uint8_t> _Payload = R_msg.Serializer();
+                        uint32_t size = _Payload.size();
+                        binding.Send(&size, sizeof(size));
+                        binding.Send(_Payload.data(), size);
+                        binding.CloseSocket();
                     }
 
                 private:
                     template <typename Class, typename R, typename... Args, std::size_t... index>
                     void sHandleCall(Class &c,
-                                      std::future<R> (Class::*method)(Args...),
+                                     std::future<R> (Class::*method)(Args...),
                                      SOMEIP_MESSAGE::Message msg,
                                      Socket &binding,
                                      std::index_sequence<index...>)
                     {
                         SOMEIP_MESSAGE::Message R_msg(
-                            SOMEIP_MESSAGE::Message_ID{ this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
-                            SOMEIP_MESSAGE::Request_ID{5,6},
+                            SOMEIP_MESSAGE::Message_ID{this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
+                            SOMEIP_MESSAGE::Request_ID{5, 6},
                             2, // protocol version
                             7, // Interface Version
                             SOMEIP_MESSAGE::MessageType::RESPONSE);
-                            
+
                         Marshal<Args...> unmarshaller(msg.GetPayload());
                         std::future<R> result = (c.*method)(unmarshaller.template unmarshal<index>()...);
                         R rval = result.get();
@@ -294,9 +286,9 @@ namespace ara
                                      Socket binding,
                                      std::index_sequence<index...>)
                     {
-                       SOMEIP_MESSAGE::Message R_msg(
-                            SOMEIP_MESSAGE::Message_ID{ this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
-                            SOMEIP_MESSAGE::Request_ID{5,6},
+                        SOMEIP_MESSAGE::Message R_msg(
+                            SOMEIP_MESSAGE::Message_ID{this->m_service_id.GetInstanceId(), msg.MessageId().method_id},
+                            SOMEIP_MESSAGE::Request_ID{5, 6},
                             2, // protocol version
                             7, // Interface Version
                             SOMEIP_MESSAGE::MessageType::RESPONSE);
