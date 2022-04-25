0e89b1270acf707014f1803b1b2ed5e55360cbeb
Bassant-Yasser <bassantyasser229@gmail.com>
2022-04-25 16:07:56 +0200

    adding threads to ucm client

--- test_partial_processes/SD_TEST/ucm_client/ucm_client.cpp
+++ test_partial_processes/SD_TEST/ucm_client/ucm_client.cpp
@@ -1,5 +1,25 @@
 #include "iostream"
-#include "ara/ucm/pkgmgr/packagemanagement_skeleton.hpp"
+#include "ara/ucm/pkgmgr/ucm_return_types.hpp"
+#include <signal.h>
+#include "ara/SD_c/proxy.hpp"
+#include <iostream>
+#include "color/color.hpp"
+#include <arpa/inet.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "ara/com/ipc/client/socket_Client.hpp"
+#include "ara/com/ipc/server/socket_Server.hpp"
+#include "ara/com/SOMEIP/SomeipSDMessage.hpp"
+#include "ara/com/SOMEIP/entry/eventgroup_entry.hpp"
+#include "ara/ucm/pkgmgr/packagemanagement_proxy.hpp"
+
+#define SD_PORT 1690
+#define SERVICE_ID 45
+#define UDP_PORT_EVENTS  7575
+#define NUM_THREADS 3
+
+using namespace std;
 
 std::vector<char> ReadAllBytes(char const *filename)
 {
@@ -10,73 +30,91 @@ std::vector<char> ReadAllBytes(char const *filename)
     ifs.read(&result[0], pos);
     return result;
 }
+// /**
+//  * @brief handle Event in client
+//  *
+//  *
+//  *
+//  * @todo
+//  *      - use serialzer and deserializer
+//  *      - get the second value which is event id then send it to the corresponding handler
+//  *
+//  * @param sigtype
+//  */
 
 
-#include <signal.h>
-#include "ara/ucm/pkgmgr/packagemanagement_proxy.hpp"
-#include "ara/ucm/pkgmgr/ucm_return_types.hpp"
+// CClient client_event_h (SOCK_DGRAM);
+// ara::com::proxy_skeleton::proxy::ServiceProxy::SP_Handle proxy_handler;
 
-#include <iostream>
-#include "color/color.hpp"
-#include <arpa/inet.h>
-#include <sys/mman.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>	
-#include "ara/com/ipc/client/socket_Client.hpp"
-#include "ara/com/ipc/server/socket_Server.hpp"
-#define SD_PORT 1690
-#define SERVICE_ID 45
-#define UDP_PORT_EVENTS  7575
-
-CClient client_event_h (SOCK_DGRAM);
-CServer ssevent(SOCK_DGRAM);
-ara::com::proxy_skeleton::proxy::ServiceProxy::SP_Handle proxy_handler;
-ara::com::FindServiceHandle findhandle{SERVICE_ID, 45, SD_PORT};
-
-//static ara::ucm::pkgmgr::proxy::PackageManagementProxy *server_proxy_ptr;
 std::shared_ptr<ara::ucm::pkgmgr::proxy::PackageManagementProxy> server_proxy_ptr ;
-// static CClient *client_event_ptr;
 Color::Modifier green(Color::FG_GREEN);
 Color::Modifier def(Color::FG_DEFAULT);
 
-/**
- * @brief handle Event in client
- *
- *
- *
- * @todo
- *      - use serialzer and deserializer
- *      - get the second value which is event id then send it to the corresponding handler
- *
- * @param sigtype
- */
+
+
+CServer ssevent(SOCK_DGRAM);
+ara::com::FindServiceHandle findhandle{SERVICE_ID, 45, SD_PORT};
+
+Color::Modifier green(Color::FG_GREEN);
+Color::Modifier def(Color::FG_DEFAULT);
+
+void Event_Handler();
+
+void *pthread0(void *v_var);
+void *pthread1(void *v_var);
 
 int main(int argc, char **argv)
-{    
+{
+    ssevent.OpenSocket(UDP_PORT_EVENTS);
+    ssevent.BindServer();
+
     ara::com::proxy_skeleton::proxy::ServiceProxy::SP_Handle hand = (ara::ucm::pkgmgr::proxy::PackageManagementProxy::FindService(findhandle)[0]);
     hand.UDP_port = UDP_PORT_EVENTS;
     server_proxy_ptr = std::make_shared<ara::ucm::pkgmgr::proxy::PackageManagementProxy>(hand);
-
-    ssevent.OpenSocket(UDP_PORT_EVENTS);
-    ssevent.BindServer();
 
 
     std::cout << "handle : " << hand.m_server_com.port_number << " " << hand.m_server_com.service_id << std::endl;
 
     std::cout << "\t\t\t[CLIENT] starting\n";
     sleep(2);
-    client_event_h.OpenSocket();
     std::cout << "\t\t\t[CLIENT] starting\n";
 
+    /*thread*/
+    pthread_t threads[NUM_THREADS];
 
-    std::string sma = "heloo";
+    int th1, th2;
+    int i = 0;
+
+    th1 = pthread_create(&threads[0], NULL, pthread0, (void *)&i);
+    if (th1)
+    {
+        cout << "ERROR" << th1 << endl;
+        exit(-1);
+    }
+    i = 1;
+    th2 = pthread_create(&threads[1], NULL, pthread1, (void *)&i);
+    if (th2)
+    {
+        cout << "ERRor" << th2 << endl;
+        exit(-1);
+    }
+
+    pthread_exit(NULL);
+
+    return 0;
+}
+/*******************************************************************************************************************
+ * @brief 
+ * 
+ * @param v_var 
+ * @return void* 
+ */
+void *pthread0(void *v_var)
+{
     std::cout << green;
     pid_t pid = getpid();
     std::cout << "\t\t\t[CLIENT] receiver: PID is " << pid << std::endl;
     std::future<ara::ucm::pkgmgr::PackageManagement::TransferStartOutput> result; /* Saves TransferStartOutput */
-
-    // Event
-    // server_proxy_obj->FindService(32);
 
     std::cout << "\t\t\t[CLIENT] Result of TransferStart : ";
     std::vector<char> Temp_data = ReadAllBytes("../../../myfile.zip");
@@ -97,10 +135,10 @@ int main(int argc, char **argv)
             }
             else
                 small_data.push_back(Temp_data[i*transfer_start_output.BlockSize + j]);
-        }  
-        
+        }
+
         // don't change 64 (packagemanagementskeleton.cpp)
-        std::future<ara::ucm::pkgmgr::PackageManagement::TransferDataOutput> result2 = 
+        std::future<ara::ucm::pkgmgr::PackageManagement::TransferDataOutput> result2 =
         server_proxy_ptr->TransferData(transfer_start_output.id, small_data, block_counter);   /* Saves TransferDataOutput */
 
         ara::ucm::pkgmgr::PackageManagement::TransferDataOutput result3;
@@ -117,9 +155,64 @@ int main(int argc, char **argv)
         printf(".\n");
         fflush(stdout);
     }
-    
-
-    return 0;
 }
+/**********************************************************************************************************************
+ * @brief handle all event and field updates
+ *
+ * @param sigtype
+ */
+void *pthread1(void *v_var)
+{
+    while (1)
+        Event_Handler();
+}
+void Event_Handler()
+{
+    // sockaddr_in echoClntAddr;                    /* Address of datagram source */
+    // unsigned int clntLen = sizeof(echoClntAddr); /* Address length */
 
+    // // ara::com::proxy_skeleton::event_info evr;
+    // std::vector<uint8_t> msg;
+    // uint32_t msg_size;
 
+    // ssevent.UDPRecFrom((void *)&msg_size, sizeof(msg_size), (struct sockaddr *)&echoClntAddr, &clntLen);
+    // msg.reserve(msg_size);
+    // ssevent.UDPRecFrom((void *)&msg[0], msg_size, (struct sockaddr *)&echoClntAddr, &clntLen);
+    // printf("\t[CLIENT]  ->> Handling client %s %d\n", inet_ntoa(echoClntAddr.sin_addr), echoClntAddr.sin_port);
+    // fflush(stdout);
+
+    // ara::com::proxy_skeleton::Client_udp_Info cudp;
+    // cudp.port = echoClntAddr.sin_port;
+    // cudp.addr = std::string(inet_ntoa(echoClntAddr.sin_addr));
+    // ara::com::SOMEIP_MESSAGE::Message Nmsg = ara::com::SOMEIP_MESSAGE::Message::Deserialize(msg);
+    // msg = Nmsg.GetPayload();
+
+    // switch (Nmsg.MessageId().serivce_id)
+    // {
+    // case SERVICE_ID:
+    //     switch (Nmsg.MessageId().method_id & 0x7FFF)
+    //     {
+    //     case 0:
+    //         server_proxy_ptr->ev1.handlecall(msg);
+    //         std::cout << "NEW EVENT1 : " << server_proxy_ptr->ev1.get_value() << std::endl;
+    //         break;
+    //     case 1:
+    //         server_proxy_ptr->ev2.handlecall(msg);
+    //         std::cout << "NEW EVENT2 : " << server_proxy_ptr->ev2.get_value() << std::endl;
+
+    //         break;
+    //     case 2:
+    //         server_proxy_ptr->fd1.handlecall(msg);
+    //         std::cout << "NEW FIELD1 : " << server_proxy_ptr->fd1.get_value() << std::endl;
+
+    //         break;
+
+    //     default:
+    //         break;
+    //     }
+    //     break;
+
+    // default:
+    //     break;
+    // }
+}
