d1491b7ceccdd1f4428c0795e4cf7b12d5236bbb
renad-mostafa99 <74263134+renad-mostafa99@users.noreply.github.com>
2022-04-27 14:58:28 +0200

    Create keyslot.hpp

---
+++ include/ara/crypto/keys/keyslot.hpp
@@ -0,0 +1,15 @@
+#ifndef ARA_CRYPTO_KEYS_KEYSLOT_H_
+#define ARA_CRYPTO_KEYS_KEYSLOT_H_
+
+namespace ara
+{
+    namespace crypto
+    {
+        class KeySlot 
+        {
+            
+        };
+
+    }
+}
+#endif
