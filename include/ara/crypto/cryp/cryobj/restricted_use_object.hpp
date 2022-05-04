3f998f0534e30b01cc86da5d68476836c77123ef
Yasmien Abdelaziz <52866187+yasmienabdelaziz@users.noreply.github.com>
2022-05-04 13:33:16 +0200

    Update restricted_use_object.hpp

--- include/ara/crypto/cryp/cryobj/restricted_use_object.hpp
+++ include/ara/crypto/cryp/cryobj/restricted_use_object.hpp
@@ -19,6 +19,8 @@ namespace ara
             class RestrictedUseObject : public CryptoObject 
             {
                 public:
+                
+                vector <byte> Key;
                 //
                 /**
                  * @brief Alias to the container type for bit-flags of allowed usages of the object
