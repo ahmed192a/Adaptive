1d43a23667c414082911cd0fd7f9dc148532e43f
Ali Abdelhafez <42843726+aliabdelhafez@users.noreply.github.com>
2022-05-04 08:39:53 +0200

    Update crypto_provider.hpp

--- include/ara/crypto/cryp/crypto_provider.hpp
+++ include/ara/crypto/cryp/crypto_provider.hpp
@@ -1,57 +1,20 @@
-#ifndef CRYPTO_PROVIDER_H_
-#define CRYPTO_PROVIDER_H_
-
-#include "ara/crypto/common/volatile_trusted_container.hpp"
-#include "ara/crypto/cryp/HMAC.hpp"
+//#ifndef CRYPTO_PROVIDER_H_
+//#define CRYPTO_PROVIDER_H_
+#pragma once
+#include "../common/concrete_io_interface.hpp"
+#include "cryobj/symmetric_key.hpp"
+//#include "volatile_trusted_container.hpp"
+#include "HMAC.hpp"
 #include "../common/vendor_specific_algorithm_identifier.hpp"
 //#include "ara/crypto/cryp/message_authn_code_ctx.hpp"
-//#include "ara/crypto/cryp/symmetric_key_wrapper_ctx.hpp"
+#include "Keywrapper.hpp"
 //#include "ara/crypto/cryp/key_derivation_function_ctx.hpp"
-//#include "ara/crypto/cryp/auth_cipher_ctx.hpp"
-//#include "ara/crypto/cryp/cryobj/signature.hpp"
-//#include "ara/crypto/cryp/random_generator_ctx.hpp"
-//#include "ara/crypto/cryp/hash_function_ctx.hpp"
-//#include "ara/crypto/cryp/symmetric_block_cipher_ctx.hpp"
-//
-#include "ara/crypto/cryp/key_derivation_function_ctx.hpp"
-
-
-			///////////////////////////////////////////
-			//			Dummy Definitions
-			///////////////////////////////////////////
-
-            class AuthCipherCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<AuthCipherCtx>;
-            };
-            class HashFunctionCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<HashFunctionCtx>;
-            };
-            class RandomGeneratorCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<RandomGeneratorCtx>;
-            };
-
-            class SymmetricBlockCipherCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<SymmetricBlockCipherCtx>;
-            };
-            class SymmetricKeyWrapperCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<SymmetricKeyWrapperCtx>;
-            };
-            /*class KeyDerivationFunctionCtx : public CryptoContext {
-            public:
-            	using Uptr = std::unique_ptr<KeyDerivationFunctionCtx>;
-            };*/
-
-
-			///////////////////////////////////////////
-			///////////////////////////////////////////
-			///////////////////////////////////////////
-
-
+#include "authentication.hpp"
+#include "cryobj/signature.hpp"
+#include "PRNG.hpp"
+#include "hash_function_ctx.hpp"
+#include "symmetric_cipher.hpp"
+//#include "cryobj/sec_seed.hpp"
 #include <string>
 namespace ara
 {
@@ -70,7 +33,7 @@ namespace ara
 
 				virtual VolatileTrustedContainer::Uptr AllocVolatileContainer (std::size_t capacity=0) noexcept=0;
 
-				virtual VolatileTrustedContainer::Uptr AllocVolatileContainer (std::pair< AlgId, CryptoObjectType > theObjectDef) noexcept=0;
+				//virtual VolatileTrustedContainer::Uptr AllocVolatileContainer (std::pair< AlgId, CryptoObjectType > theObjectDef) noexcept=0;
 
 				virtual AlgId ConvertToAlgId (std::string primitiveName) const noexcept=0;
 
@@ -82,17 +45,39 @@ namespace ara
 
 				virtual HashFunctionCtx::Uptr CreateHashFunctionCtx (AlgId algId) noexcept=0;
 
-				virtual KeyDerivationFunctionCtx::Uptr CreateKeyDerivationFunctionCtx (AlgId algId) noexcept=0;
+				//virtual KeyDerivationFunctionCtx::Uptr CreateKeyDerivationFunctionCtx (AlgId algId) noexcept=0;
 
 				virtual MessageAuthnCodeCtx::Uptr CreateMessageAuthCodeCtx (AlgId algId) noexcept=0;
 
-				virtual RandomGeneratorCtx::Uptr CreateRandomGeneratorCtx (AlgId algId=kAlgIdDefault, bool initialize=true) noexcept=0;
+				virtual PRNG::Uptr CreateRandomGeneratorCtx (AlgId algId=kAlgIdDefault, bool initialize=true) noexcept=0;
 
-				virtual Signature::Uptrc CreateSignature (AlgId signAlgId, ReadOnlyMemRegion value, const RestrictedUseObject &key,AlgId hashAlgId=kAlgIdNone) noexcept=0;
+				//virtual Signature::Uptrc CreateSignature (AlgId signAlgId, ReadOnlyMemRegion value, const RestrictedUseObject &key,AlgId hashAlgId=kAlgIdNone) noexcept=0;
 
 				virtual SymmetricBlockCipherCtx::Uptr CreateSymmetricBlockCipherCtx (AlgId algId) noexcept=0;
 
-				virtual SymmetricKeyWrapperCtx::Uptr CreateSymmetricKeyWrapperCtx (AlgId algId) noexcept=0;
+				virtual Keywrapper::Uptr CreateSymmetricKeyWrapperCtx (AlgId algId) noexcept=0;
+
+				virtual std::vector<uint8_t>  ExportPublicObject (const ConcreteIOInterface &container) noexcept=0;
+
+				virtual std::vector<uint8_t>  ExportSecuredObject (const ConcreteIOInterface &container, SymmetricKeyWrapperCtx &transportContext) noexcept=0;
+
+				virtual SecSeed::Uptrc GenerateSeed (AlgId algId, SecSeed::Usage allowedUsage, bool isSession=true, bool isExportable=false) noexcept=0;
+
+				virtual SymmetricKey::Uptrc GenerateSymmetricKey(AlgId algId, AllowedUsageFlags allowedUsage, bool isSession=true,bool isExportable=false) noexcept=0;
+
+				virtual std::size_t GetPayloadStorageSize (CryptoObjectType cryptoObjectType, AlgId algId) const noexcept=0;
+
+				virtual std::size_t GetSerializedSize (CryptoObjectType cryptoObjectType, AlgId algId) const noexcept=0;
+
+				virtual void ImportPublicObject (ConcreteIOInterface &container, ReadOnlyMemRegion serialized, CryptoObjectType expectedObject=CryptoObjectType::kUndefined) noexcept=0;
+
+				virtual void ImportSecuredObject (ConcreteIOInterface &container, ReadOnlyMemRegion serialized, SymmetricKeyWrapperCtx &transportContext, bool isExportable=false, CryptoObjectType expectedObject=CryptoObjectType::kUndefined) noexcept=0;
+
+				virtual CryptoObject::Uptrc LoadObject (const ConcreteIOInterface &container) noexcept=0;
+
+				virtual SecretSeed::Uptrc LoadSecretSeed (const ConcreteIOInterface &container) noexcept=0;
+
+				virtual SymmetricKey::Uptrc LoadSymmetricKey (const ConcreteIOInterface &container) noexcept=0;
 
 				virtual ~CryptoProvider () noexcept=default;
 			};
@@ -103,4 +88,4 @@ namespace ara
 
 
 
-#endif /* CRYPTO_PROVIDER_H */
+//#endif /* CRYPTO_PROVIDER_H */
