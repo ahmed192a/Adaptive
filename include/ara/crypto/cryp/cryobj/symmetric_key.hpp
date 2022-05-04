a44cebaeae38230de86d757d95111d0a8299d422
renad-mostafa99 <74263134+renad-mostafa99@users.noreply.github.com>
2022-05-04 17:05:00 +0200

    Update symmetric_key.hpp

--- include/ara/crypto/cryp/cryobj/symmetric_key.hpp
+++ include/ara/crypto/cryp/cryobj/symmetric_key.hpp
@@ -1,15 +1,44 @@
-#ifndef ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
-#define ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
+//#ifndef ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
+///#define ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
+#pragma once
+#include "rest_use_obj.hpp"
 
-#include "ara/crypto/cryp/cryobj/restricted_use_object.hpp"
+namespace ara
+{
+    namespace crypto
+    {
+        namespace cryp
+        {
+//class RestrictedUseObject;
+class SymmetricKey : public RestUseObj {
+public:
+using Uptrc = std::unique_ptr<const SymmetricKey>;
 
+                //SWS_CRYPT_23002
+                /**
+                 * @brief Unique smart pointer of a volatile interface instance.
+                 * 
+                 */
+                using Uptr = std::unique_ptr<SymmetricKey>;
+static const CryptoObjectType kObjectType = CryptoObjectType::kSymmetricKey;
+uint32_t Seed;
+std::vector<uint8_t> keyVal;
+using Usage = AllowedUsageFlags;
+                Usage allowed;
+                bool Volatile;
+                bool session;
+                bool exportable;
+                SymmetricKey(AllowedUsageFlags allowedVal,bool sessionVar,bool exportableVar)
+                {
+                    allowed=allowedVal;
+                    session=sessionVar;
+                    exportable=exportableVar;
 
-class SymmetricKey : public RestrictedUseObject {
-
-
-
-
-
+                } 
+                ~SymmetricKey() noexcept;
 };
+        }
+    }
+}
 
-#endif // ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
+//#endif // ARA_CRYPTO_CRYP_SYMMETRIC_KEY_H
