d5814475bce13a34e3fb91988b92d21f5f187a5f
Nadalotfy44 <73910708+Nadalotfy44@users.noreply.github.com>
2022-05-05 21:40:20 +0200

    Update keyslot.hpp

--- include/ara/crypto/keys/keyslot.hpp
+++ include/ara/crypto/keys/keyslot.hpp
@@ -17,7 +17,8 @@ namespace ara
             {
                 closed,
                 opened,
-                commited
+                commited,
+                empty
             };
 
             class KeySlot 
