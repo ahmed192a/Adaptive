e7b714d6a39bcd785ee5b814e97c0828b5f62c23
renad-mostafa99 <74263134+renad-mostafa99@users.noreply.github.com>
2022-05-05 17:12:30 +0200

    Update key_slot_content_props.hpp

--- include/ara/crypto/keys/key_slot_content_props.hpp
+++ include/ara/crypto/keys/key_slot_content_props.hpp
@@ -1,4 +1,3 @@
-
 #ifndef ARA_CRYPTO_KEYS_KEY_SLOT_CONTENT_PROPS_HPP
 #define ARA_CRYPTO_KEYS_KEY_SLOT_CONTENT_PROPS_HPP
 #include <iostream>
@@ -14,25 +13,56 @@ namespace ara
 
 		//## A structure contains all the properties of the key slot.
 		struct KeySlotContentProps {
+			
+			//Cryptoalgorithm of actual object stored to the slot.
+				CryptoAlgId mAlgId;
 
-		//Cryptoalgorithm of actual object stored to the slot.
-			CryptoAlgId mAlgId;
+			//Actual size of an object currently stored to the slot.
+				std::size_t mObjectSize;
 
-		//Actual size of an object currently stored to the slot.
-			std::size_t mObjectSize;
+			//Actual type of an object stored to the slot.
+				CryptoObjectType mObjectType;
 
-		//Actual type of an object stored to the slot.
-			CryptoObjectType mObjectType;
+			//UID of a Crypto Object stored to the slot.
+				CryptoObjectUid mObjectUid;
 
-		//UID of a Crypto Object stored to the slot.
-			CryptoObjectUid mObjectUid;
+			//Actual usage restriction flags of an object stored to the slot for the current "Actor".
+				AllowedUsageFlags mContentAllowedUsage;
+				/**
+				 * @brief unique pointer
+				 * 
+				 */
+			using Uptr = std::unique_ptr<KeySlotContentProps>;
+			/**
+			 * @brief Construct a new Key Slot Content Props object
+			 * 
+			 */
+			KeySlotContentProps ()=default;
+			/**
+			 * @brief Comparison operator "equal" for KeySlotContentProps operands.
+			 * 
+			 * @param lhs 
+			 * @param rhs 
+			 * @return true 
+			 * @return false 
+			 */
+			friend bool operator== (const KeySlotContentProps &lhs, const KeySlotContentProps &rhs) noexcept;
+			
+			/**
+			 * @brief Comparison operator "not equal" for KeySlotContentProps operands.
+			 * 
+			 * @param lhs 
+			 * @param rhs 
+			 * @return true 
+			 * @return false 
+			 */
+			friend bool operator!= (const KeySlotContentProps &lhs, const KeySlotContentProps &rhs) noexcept;
 
-		//Actual usage restriction flags of an object stored to the slot for the current "Actor".
-			AllowedUsageFlags mContentAllowedUsage;
 
-		};
-		using Uptr = std::unique_ptr<KeySlotContentProps>;
 
+
+			};
+		
 		}
 	}
 }
