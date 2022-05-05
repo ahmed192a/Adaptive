eea6a3e5ef7e983a4c63c2ac1b93a7bca155e056
nehalyasser <nehalyasser1999@gmail.com>
2022-05-05 14:44:17 +0200

    fixed cryptoContext

--- include/ara/crypto/cryp/crypto_context.hpp
+++ include/ara/crypto/cryp/crypto_context.hpp
@@ -1,20 +1,20 @@
-#ifndef ARA_CRYPTO_CRYPTO_CONTEXT_H
-#define ARA_CRYPTO_CRYPTO_CONTEXT_H
-
+//#ifndef ARA_CRYPTO_CRYPTO_CONTEXT_H
+//#define ARA_CRYPTO_CRYPTO_CONTEXT_H
+#pragma once
 #include <iostream>
 #include <memory>
 
 #include "../cryp/cryobj/crypto_primitive_id.hpp"
 #include "../cryp/cryobj/crypto_pr_id.hpp"
-#include "../cryp/crypto_provider.hpp"
+//#include "../cryp/crypto_provider.hpp"
 
 namespace ara {
-    namespace crypto {
-        namespace cryp {
-
-            class CryptoContext
-            {
-            public:
+	namespace crypto {
+		namespace cryp {
+			class CryptoProvider;
+			class CryptoContext
+			{
+			public:
 				using AlgId = CryptoAlgId;
 				/* Destructor */
 				virtual ~CryptoContext() noexcept = default;
@@ -26,18 +26,18 @@ namespace ara {
 				virtual bool IsInitialized() const noexcept = 0;
 
 				/* Copy - assign another CryptoContext to this instance */
-				CryptoContext& operator= (const CryptoContext & other) = default;
+				CryptoContext& operator= (const CryptoContext& other) = default;
 
 				/*/Move - assign another CryptoContext to this instance */
-				CryptoContext& operator= (CryptoContext && other) = default;
+				CryptoContext& operator= (CryptoContext&& other) = default;
 
 				/* Get a reference to Crypto Provider of this context*/
 				virtual CryptoProvider& MyProvider() const noexcept = 0;
 
-            };
+			};
 
-        }
-    }
+		}
+	}
 }
 
-#endif
+//#endif
