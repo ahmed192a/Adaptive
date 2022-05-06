6ff5796d3bf194c233953551d643766febca6035
Rawan Khaled <67297151+rere-kh1999@users.noreply.github.com>
2022-05-06 02:37:05 +0200

    Update Inher_key_slot.hpp

--- include/ara/crypto/keys/Inher_key_slot.hpp
+++ include/ara/crypto/keys/Inher_key_slot.hpp
@@ -24,20 +24,20 @@ namespace ara
                *
                * @return ara::core::Result<KeySlotContentProps>
                */
-                ara::core::Result<KeySlotContentProps> GetContentProps ()const noexcept=0;
+                KeySlotContentProps GetContentProps ()const noexcept=0;
                 /**
                 *  @brief Retrieve an instance of the CryptoProvider that owns this KeySlot. Any key slot always has an
                 *  associated default Crypto Provider that can serve this key slot. In the simplest case all key slots
                 * can be served by a single Crypto Provider installed on the Adaptive Platform.
                 * @return ara::core::Result<cryp::CryptoProvider::Uptr>
                 */
-                ara::core::Result<cryp::CryptoProvider::Uptr> MyProvider ()const noexcept=0;
+                cryp::CryptoProvider::Uptr MyProvider ()const noexcept=0;
              
                 /**
                 * @brief Clear the content of this key-slot.
                 *
                 **/
-               ara::core::Result<void> Clear () noexcept=0;
+               void Clear () noexcept=0;
             };
         }
     }
