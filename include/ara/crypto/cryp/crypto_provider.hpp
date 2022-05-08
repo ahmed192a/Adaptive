927694316961de99702bc4b409056a2d5b3de3bc
BaSsioonii <63972053+BaSsioonii@users.noreply.github.com>
2022-05-08 17:12:51 +0200

    Update crypto_provider.hpp

--- include/ara/crypto/cryp/crypto_provider.hpp
+++ include/ara/crypto/cryp/crypto_provider.hpp
@@ -8,13 +8,13 @@
 #include "../common/vendor_specific_algorithm_identifier.hpp"
 //#include "ara/crypto/cryp/message_authn_code_ctx.hpp"
 #include "Keywrapper.hpp"
-#include "ara/crypto/cryp/HKDF.hpp"
+#include "HKDF.hpp"
 #include "authentication.hpp"
 #include "cryobj/signature.hpp"
 #include "PRNG.hpp"
-#include "hash_function_ctx.hpp"
+#include "hash_service.hpp"
 #include "symmetric_cipher.hpp"
-//#include "cryobj/sec_seed.hpp"
+#include "cryobj/sec_seed.hpp"
 #include <string>
 namespace ara
 {
@@ -41,9 +41,9 @@ namespace ara
 
 				virtual Authentication::Uptr CreateAuthCipherCtx (AlgId algId) noexcept=0;
 
-				virtual Signature::Uptrc CreateHashDigest (AlgId hashAlgId, ReadOnlyMemRegion value) noexcept=0;
+				//virtual Signature::Uptrc CreateHashDigest (AlgId hashAlgId, ReadOnlyMemRegion value) noexcept=0;
 
-				virtual HashFunctionCtx::Uptr CreateHashFunctionCtx (AlgId algId) noexcept=0;
+				virtual HashService::Uptr CreateHashFunctionCtx (AlgId algId) noexcept=0;
 
 				virtual HKDF::Uptr CreateKeyDerivationFunctionCtx (AlgId algId) noexcept=0;
 
@@ -51,7 +51,7 @@ namespace ara
 
 				virtual PRNG::Uptr CreateRandomGeneratorCtx (AlgId algId=kAlgIdDefault, bool initialize=true) noexcept=0;
 
-				//virtual Signature::Uptrc CreateSignature (AlgId signAlgId, ReadOnlyMemRegion value, const RestrictedUseObject &key,AlgId hashAlgId=kAlgIdNone) noexcept=0;
+				virtual Sign::Uptrc CreateSignature (AlgId signAlgId, ReadOnlyMemRegion value, const RestrictedUseObject &key,AlgId hashAlgId=kAlgIdNone) noexcept=0;
 
 				virtual SymmetricCipher::Uptr CreateSymmetricBlockCipherCtx (AlgId algId) noexcept=0;
 
@@ -59,7 +59,7 @@ namespace ara
 
 				virtual std::vector<uint8_t>  ExportPublicObject (const ConcreteIOInterface &container) noexcept=0;
 
-				virtual std::vector<uint8_t>  ExportSecuredObject (const ConcreteIOInterface &container, SymmetricKeyWrapperCtx &transportContext) noexcept=0;
+				//virtual std::vector<uint8_t>  ExportSecuredObject (const ConcreteIOInterface &container, SymmetricKeyWrapperCtx &transportContext) noexcept=0;
 
 				virtual SecSeed::Uptrc GenerateSeed (AlgId algId, SecSeed::Usage allowedUsage, bool isSession=true, bool isExportable=false) noexcept=0;
 
@@ -67,19 +67,19 @@ namespace ara
 
 				virtual std::size_t GetPayloadStorageSize (CryptoObjectType cryptoObjectType, AlgId algId) const noexcept=0;
 
-				virtual std::size_t GetSerializedSize (CryptoObjectType cryptoObjectType, AlgId algId) const noexcept=0;
+				//virtual std::size_t GetSerializedSize (CryptoObjectType cryptoObjectType, AlgId algId) const noexcept=0;
 
 				virtual void ImportPublicObject (ConcreteIOInterface &container, ReadOnlyMemRegion serialized, CryptoObjectType expectedObject=CryptoObjectType::kUndefined) noexcept=0;
 
-				virtual void ImportSecuredObject (ConcreteIOInterface &container, ReadOnlyMemRegion serialized, SymmetricKeyWrapperCtx &transportContext, bool isExportable=false, CryptoObjectType expectedObject=CryptoObjectType::kUndefined) noexcept=0;
+				virtual void ImportSecuredObject (ConcreteIOInterface &container, ReadOnlyMemRegion serialized, Keywrapper &transportContext, bool isExportable=false, CryptoObjectType expectedObject=CryptoObjectType::kUndefined) noexcept=0;
 
-				virtual CryptoObject::Uptrc LoadObject (const ConcreteIOInterface &container) noexcept=0;
+				virtual cryptoobj::Uptrc LoadObject (const ConcreteIOInterface &container) noexcept=0;
 
-				virtual SecretSeed::Uptrc LoadSecretSeed (const ConcreteIOInterface &container) noexcept=0;
+				virtual SecSeed::Uptrc LoadSecretSeed (const ConcreteIOInterface &container) noexcept=0;
 
 				virtual SymmetricKey::Uptrc LoadSymmetricKey (const ConcreteIOInterface &container) noexcept=0;
 
-				virtual ~CryptoProvider () noexcept=default;
+				
 			};
 		}
 	}
