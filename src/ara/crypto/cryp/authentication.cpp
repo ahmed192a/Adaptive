cc858a9ba771b83525930a960e69d6537449be5d
nehalyasser <nehalyasser1999@gmail.com>
2022-05-17 18:52:10 +0200

    solving an error

--- src/ara/crypto/cryp/authentication.cpp
+++ src/ara/crypto/cryp/authentication.cpp
@@ -141,7 +141,7 @@ void Authentication:: UpdateAssociatedData(std::uint8_t in) noexcept
 		// update the digest through the Update function of the mac context in this authentication instance
 		this->macPtr->Update(in);
 
-		this->status == AuthCipherCtx_Status::updated;
+		this->status = AuthCipherCtx_Status::updated;
 	}
 
 	//// UpdateAssociatedData raises error if ProcessConfedentialData has been already called 
