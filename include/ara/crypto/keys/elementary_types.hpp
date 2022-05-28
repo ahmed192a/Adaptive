a60ee7524bc945afbb67715ad20a4486cbca3c41
nehalyasser <nehalyasser1999@gmail.com>
2022-05-28 18:55:29 +0200

    Build elementary types

--- include/ara/crypto/keys/elementary_types.hpp
+++ include/ara/crypto/keys/elementary_types.hpp
@@ -1,44 +1,46 @@
-// #ifndef ARA_CRYPTO_KEYS_ELEMENTARY_ELEMENTS_HPP
-// #define ARA_CRYPTO_KEYS_ELEMENTARY_ELEMENTS_HPP
+ #ifndef ARA_CRYPTO_KEYS_ELEMENTARY_ELEMENTS_HPP
+ #define ARA_CRYPTO_KEYS_ELEMENTARY_ELEMENTS_HPP
 
-// #include <vector>
-// #include "keyslot.hpp"
+#include <vector>
+//#include "keyslot.hpp"
+#include "Inher_key_slote.hpp"
 
-// namespace ara
-// {
-// 	namespace crypto
-// 	{
-// 		namespace keys
-// 		{
+ namespace ara
+ {
+ 	namespace crypto
+ 	{
+ 		namespace keys
+ 		{
 
-// 			//@breif: Definition of a transaction identifier type. 
-// 			// The zero value should be reserved for especial cases.
-// 			using TransactionId = std::uint64_t;
+ 			//@breif: Definition of a transaction identifier type. 
+ 			// The zero value should be reserved for especial cases.
+ 			using TransactionId = std::uint64_t;
 
-// 			//@breif: The "transaction scope" is a list of key slots that are target for update in a transaction
-// 			using TransactionScope = std::vector<KeySlot>;
+ 			//@breif: The "transaction scope" is a list of key slots that are target for update in a transaction
+ 			using TransactionScope = std::vector<InhKeySlot>;
 
-// 			/************************************************************************************/
-// 			/********* Not in SWS so may be remeoved later if better solution is found **********/
-// 			/************************************************************************************/
+ 			/************************************************************************************/
+ 			/********* Not in SWS so may be remeoved later if better solution is found **********/
+ 			/************************************************************************************/
 			
-// 			enum class TransactionScopeState : std::uint8_t
-// 			{
-// 				opened,
-// 				commited,
-// 				rolledback
-// 			};
+ 			enum class TransactionScopeState : std::uint8_t
+ 			{
+ 				opened,
+ 				commited,
+ 				rolledback
+ 			};
 
-// 			//@breif: a pair to keep track of opened TransactionScopes which are ready to be commited with changes 
-// 			//        combined with their TransactionId s
-// 			struct OpenedTransactionScopeWithIdPair
-// 			{
-// 				TransactionScope OpenedTransaction;
-// 				TransactionId OpenedTransactionId;
-// 				TransactionScopeState OpenedTransactionState;
-// 			};
-// 		}
-// 	}
-// }
+ 			//@breif: a struct to keep track of TransactionScopes commited with changes combined with 
+            //        their TransactionId s, and stating whether these changes are ready to commited, 
+            //        already committed, or rolled back
+ 			struct TransactionScopeWithIdAndState
+ 			{
+ 				TransactionScope Transaction;
+ 				TransactionId TransactionId;
+ 				TransactionScopeState TransactionState;
+ 			};
+ 		}
+ 	}
+ }
 
-// #endif
+ #endif
