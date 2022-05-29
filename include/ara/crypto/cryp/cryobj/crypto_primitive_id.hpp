b8ce3f9d9866ba14979bbdafa88f9b8d803b540c
nehalyasser <nehalyasser1999@gmail.com>
2022-05-29 20:06:33 +0200

    add include <memory> again in cryptoPrimitiveId

--- include/ara/crypto/cryp/cryobj/crypto_primitive_id.hpp
+++ include/ara/crypto/cryp/cryobj/crypto_primitive_id.hpp
@@ -1,6 +1,6 @@
 #ifndef ARA_CRYPTO_CRYP_CRYBOBJ_
 #define ARA_CRYPTO_CRYP_CRYBOBJ_
-//#include <memory>
+#include <memory>
 #include <iostream>
 //#include "../../../core/StringView.hpp"
 #include "../../common/base_id_types.hpp"
